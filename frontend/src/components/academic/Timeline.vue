6148c45264894f65fcf4f8638e2529ed6d076ba3
krigjo25 <krigjo25@gmail.com>
2025-08-06 17:49:41 +0200

    fix: Adjust timeline component layout for improved readability and visibility of school details

--- frontend/src/components/academic/Timeline.vue
+++ frontend/src/components/academic/Timeline.vue
@@ -10,16 +10,16 @@
         <section v-show="data.content.isVisable"
             class="flex-wrap-column timeline-content">
             <h3>{{ data.title }}</h3>
-            <p>{{ data.description }}</p>
 
             <section v-if="!!data.content.school" class="flex-column-justify-center-align-center">
-                <Anchor :data="data.content.school.anchor" />
-                <p>{{ data.content.school.location }}</p>
-
+                Skole :<Anchor :data="data.content.school.anchor" />
+                <p>Campus : {{ data.content.school.location }}</p>
+                <p>År : {{ data.content.school.start }} - {{ data.content.school.end }}</p>
                 <h3 v-if="!!data.content.anchor"> 
                     <Anchor :data="data.content.anchor" />
                 </h3>
             </section>
+            <p v-if="!!data.description">{{ data.description }}</p>
         </section>
     </section>
 </template>
