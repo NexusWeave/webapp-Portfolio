cda7aaf1d56c4391ae5745ccfb63bcff82607f76
krigjo25 <krigjo25@gmail.com>
2025-08-15 14:51:31 +0200

    Add Icon component with template and props definition

---
+++ frontend/src/components/utils/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+    <span :class="cls[0]">
+        <span :class="cls[1]" :aria-label="label"></span> 
+        </span>
+</template>
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        label: {
+            type: [String],
+            required: false
+        },
+        cls: {
+            type: [Array],
+            required: false,
+        }
+
+    });
+</script>
