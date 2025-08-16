1dd340fd817d17d7f67abae025dd9afe400b9543
krigjo25 <krigjo25@gmail.com>
2025-08-16 07:33:43 +0200

    Add Year component to display year with visibility control

---
+++ frontend/src/components/Date/Year.vue
@@ -0,0 +1,28 @@
+<template>
+    <template v-if="isVisible">
+        <time :datetime="year">{{ year }}</time>
+    </template>
+    
+</template>
+<script setup>
+    import { computed, defineProps } from 'vue';
+
+    const props = defineProps({
+        year: {
+            type: Number,
+            required: true
+        },
+        isVisible: {
+            type: Boolean,
+            required: false
+        }
+    });
+
+    const year = props.year
+
+    const isVisible = computed(() => {
+        return props.isVisible !== undefined ? props.isVisible : true;
+    });
+    console.log(year, isVisible.value)
+
+</script>
