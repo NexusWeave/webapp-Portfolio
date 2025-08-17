30e869931099a375b4ad61e7112f2864efdde8a3
krigjo25 <krigjo25@gmail.com>
2025-08-17 13:38:37 +0200

    Fix references to timelineStore in Academic.vue to use academicStore for consistency

--- frontend/src/components/academic/Academic.vue
+++ frontend/src/components/academic/Academic.vue
@@ -1,36 +1,36 @@
 <template>
-    <section v-if="timeline.isLoaded" 
+    <section v-if="academic.isLoaded" 
         class="timeline-container component-blue timeline-container">
 
         <section class="flex-wrap-row-justify-space-evenly component-blue timeline-line">
             <Timeline
-            :data="timeline.timelineRange"
+            :data="academic.timelineRange"
             :cls="[['flex-column-align-items-center', 'timeline-item'],
                 'title-h2', ['timeline-input-label', 'timeline-input']]"
             @toggle-visibility="toggleVisibility"/>
         </section>
         <h3 class="timeline-h3">
-            <Year v-for="data in timeline.timelines" :key="data.id"
+            <Year v-for="data in academic.timelines" :key="data.id"
                     :year="data.year" :isVisible="data.isVisible"/>
         </h3>
         <section class="flex-wrap-row-justify-space-evenly component-blue">
-                <Card v-for="data in timeline.timelines" :key="data.id"
+                <Card v-for="data in academic.timelines" :key="data.id"
                     :data="data"/>
         </section>
     </section>  
 </template>
 <script setup>
-    import { timelineStore } from '@/stores/academicStore.js';
+    import { academicStore } from '@/stores/academicStore.js';
 
     import Year from '@/components/Date/Year.vue';
     import Card from '@/components/academic/Card.vue';
     import Timeline from '@/components/academic/Timeline.vue';
 
-    const timeline = timelineStore();
+    const academic = academicStore();
 
 
     const toggleVisibility = (id) => {
-        const data = timeline.timelines;
+        const data = academic.timelines;
         id = parseInt(id);
 
         data.forEach(item => {
