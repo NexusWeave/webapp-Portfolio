b8440e9d3b849d7091f0aae5a96575917d6a4ee0
krigjo25 <krigjo25@gmail.com>
2025-08-17 14:26:03 +0200

    Refactor Achivements.vue to use achivement data and improve visibility toggle logic

--- frontend/src/components/Achivements.vue
+++ frontend/src/components/Achivements.vue
@@ -1,41 +1,45 @@
 <template>
-    <section v-if="timeline.isLoaded" 
+    <section v-if="achivement.isLoaded" 
         class="timeline-container component-blue timeline-container">
-
-        <section class="flex-wrap-row-justify-space-evenly component-blue timeline-line">
+        
+        <section class="flex-wrap-row-justify-space-evenly timeline-line">
+            
             <Timeline
-            :data="timeline.timelineRange"
+            :data="achivement.timelineRange"
             :cls="[['flex-column-align-items-center', 'timeline-item'],
                 'title-h2', ['timeline-input-label', 'timeline-input']]"
             @toggle-visibility="toggleVisibility"/>
         </section>
-        <h3 class="timeline-h3">
-            <Year v-for="data in timeline.timelines" :key="data.id"
-                    :year="data.year" :isVisible="data.isVisible"/>
-        </h3>
+        
         <section class="flex-wrap-row-justify-space-evenly component-blue">
-                <Card v-for="data in timeline.timelines" :key="data.id"
+                <h3 class="timeline-h3">
+                    <Year v-for="data in achivement.achivements" :key="data.id"
+                        :year="data.year" :isVisible="data.isVisible"/>
+                </h3>
+        </section>
+        <section class="flex-wrap-row-justify-space-evenly component-blue">
+                <Card v-for="data in achivement.achivements" :key="data.id"
                     :data="data"/>
         </section>
-    </section>  
+
+    </section>
 </template>
 <script setup>
-    import { timelineStore } from '@/stores/academicStore.js';
+    import { achivementStore } from '@/stores/achivementsStore.js';
 
     import Year from '@/components/Date/Year.vue';
     import Card from '@/components/academic/Card.vue';
     import Timeline from '@/components/academic/Timeline.vue';
 
-    const timeline = AchivementTimelineStore();
-
+    const achivement = achivementStore();
 
     const toggleVisibility = (id) => {
-        const data = timeline.timelines;
+        const data = achivement.achivements;
         id = parseInt(id);
+        console.log(data);
 
         data.forEach(item => {
             if (item.id === id) 
-            
             {
                 item.isVisible = true;
                 console.log(item.id, id, item);
@@ -44,5 +48,6 @@
         });
         console.warn(`Toggling visibility for ID:${id}`, data[id].isVisible);
     };
-    //console.warn("Timeline data on load:", timeline.timelines);
+
+    console.warn("Timeline data on load:", achivement.achivements, achivement.isLoaded);
 </script>
