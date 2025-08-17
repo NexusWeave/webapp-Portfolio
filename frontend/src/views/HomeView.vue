c283610408d429e42e97cd21c3ae31b252ca6fd7
krigjo25 <krigjo25@gmail.com>
2025-08-17 15:42:59 +0200

    Add Portfolio component and implement technical repositories display

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,11 +6,11 @@
         <Academiic/>
     </section>
     <section>
+        <Portfolio />
     </section>
 </template>
 <script setup>
+    import Portfolio from '@/components/Portfolio.vue';
     import Achievements from '@/components/Achievements.vue';
     import Academiic from '@/components/academic/Academic.vue';
-    
-
 </script>
