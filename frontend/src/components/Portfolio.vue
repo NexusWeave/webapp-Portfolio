d4493a27546c437deb5f247ba43cc9e6a4baa259
krigjo25 <krigjo25@gmail.com>
2025-08-21 11:28:58 +0200

    Refactor Portfolio.vue to use pfolio for data handling and improve code consistency

--- frontend/src/components/Portfolio.vue
+++ frontend/src/components/Portfolio.vue
@@ -1,17 +1,18 @@
 <template>
-    <template v-if="!!portofolio.repositories">
+    <template v-if="!!pfolio.isLoaded">
         <section id="fullstack"  class="flex-wrap-column">
         <h2>Technical Repositories</h2>
         
         <section class="flex-wrap-column-align-items-center">
-            <Pagination class='flex-wrap-row-justify-space-evenly tech-bar':data="portofolio.data.Total" @update="portofolio.current = $event" v-if="portofolio.data.Total"/>
+            <Pagination class='flex-wrap-row-justify-space-evenly tech-bar':data="pfolio.data.Total" @update="pfolio.current = $event" v-if="pfolio.data.Total"/>
         </section>
 
-        <section id="technologies" class="tech-repo flex-wrap-row-justify-center">
-            <div class="flex-wrap-row" v-for="data in portofolio.displayData" :key="data.id">
+        <section class="tech-repo flex-wrap-row-justify-center">
+            
+            <div class="flex-wrap-row" v-for="data in pfolio.displayData()" :key="data.id">
                 <div class="tech-container flex-wrap-column  ">
                     <div v-for="lang in data.lang" :key="lang.id" class="img-wrapper flex-wrap-row-justify-space-between relative">
-                        <img class="img-svg" :src="'./src/assets/img/techlogo/' + lang.lang + '.svg'" :alt="lang.lang + '.svg'" />
+                        <img class="img-svg" :src="'/media/tech-lang-icons/' + lang.lang + '.svg'" :alt="lang.lang + '.svg'" />
                         <span class="time">
                             <time v-bind:datetime="data.date">{{ data.date }}</time>
                         </span>
@@ -20,7 +21,7 @@
                     <h3 v-else>{{ data.name }}</h3>
                     <p>{{ data.description }}</p>
                     <nav class="pro-nav flex-wrap-row-justify-space-evenly">
-                        <Link :link="url" v-for="url in data.links"/>
+                       
 
                     </nav>
                 </div>
@@ -28,6 +29,9 @@
         </section>
 
     </section>
+
+    {{ pfolio.displayData() }}
+
     </template>
 
     <template v-else>
@@ -45,15 +49,15 @@
 
     //  Importing components
     import Form from '@/components/form/Form.vue';
-    import Link from '@/components/navigation/Anchor.vue';
+    import Anchor from '@/components/navigation/Anchor.vue';
     
     import Pagination from '@/components/navigation/Pagination.vue';
 
 
     //  Initializing reactive objects
-    const portofolio = portfolioStore();
+    const pfolio = portfolioStore();
 
     //  Fetching data from the server
     //  Fetching the data from the server
-    console.warn("Portfolio API Response :", portofolio.repositories);
+    console.warn("Portfolio API Response :", pfolio.repositories);
 </script>
