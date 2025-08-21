8ae57c4c9d7c5b5f7fdf5e085a812443fc4d22ce
krigjo25 <krigjo25@gmail.com>
2025-08-21 15:51:14 +0200

    Refactor BusinessCard.vue to improve structure and ensure consistent class usage

--- frontend/src/components/portfolio/BusinessCard.vue
+++ frontend/src/components/portfolio/BusinessCard.vue
@@ -7,14 +7,16 @@
                 :class="[cls[2]]"
                 :src="'/media/tech-lang-icons/' + lang.lang + '.svg'" 
                 :alt="lang.lang + '.svg'" />
-                <span :class="[cls[3]]">
-                    <time v-bind:datetime="data.date">{{ data.date }}</time>
-                </span>
         </div>
         <h3 :class="[cls[4]]" v-if="Array.isArray(data.name)">{{ data.name[1] }}</h3>
         <h3 :class="[cls[4]]" v-else>{{ data.name }}</h3>
+        <span :class="[cls[3]]">
+            <time v-bind:datetime="data.date">{{ data.date }}</time>
+        </span>
+
         <p :class="[cls[5]]">{{ data.description }}</p>
-            <!--Navigation :data="url" v-for="url in data.links" :cls="[cls[6]]" /-->
+        <p :class="[cls[5]]">{{ data.description }}</p>
+        <!--Navigation :data="url" v-for="url in data.links" :cls="[cls[6]]" /-->
     </div>
 </template>
 <script setup>
@@ -27,9 +29,9 @@
         },
         cls: {
             type: Array,
-            default: () => [['card-container','flex-wrap-column'], 
-            ['figure-container', 'flex-wrap-row-justify-space-between', 'relative'],
-            'figure-img', 'date-container', 'title-h3', 'tech-description',
+            default: () => [['card-container', 'grid-container','flex-wrap-column'], 
+            ['grid-item-1', 'flex-wrap-row-justify-space-between'],
+            'figure-img', 'grid-item-3', 'tech-text', 'tech-text',
             ['nav-container', 'flex-wrap-row-justify-space-evenly']]
         }
     });
