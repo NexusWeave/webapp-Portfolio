1298c7dca48f6b9e14f1f105eb0f0f1e97b60eb9
krigjo25 <krigjo25@gmail.com>
2025-08-21 12:45:24 +0200

    Fix import path for Portfolio component in HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -13,7 +13,7 @@
     </section>
 </template>
 <script setup>
-    import Portfolio from '@/components/Portfolio.vue';
+    import Portfolio from '@/components/portfolio/Portfolio.vue';
     import Achievements from '@/components/Achievements.vue';
     import Academiic from '@/components/Academic.vue';
 </script>
