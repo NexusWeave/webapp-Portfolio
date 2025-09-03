925c89c13ff072dc9c2a7b81c44e727ebb8d1b70
krigjo25 <krigjo25@gmail.com>
2025-09-03 10:18:06 +0200

    Add AboutView component: create template and script for personal information display

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+    <article>
+        <Figure />
+        <section>
+            <h2> Kristoffer Gjøsund</h2>
+            <p> Profil informasjon om meg</p>
+        </section>
+        <section>
+            <p> Litt dypere om min reise </p>
+        </section>
+
+        <section>
+            <p> mer historie om meg</p>
+        </section>
+    </article>
+
+    <section>
+        <Repo />
+    </section>
+    <article>
+        <section>
+            <p> mer historie om meg</p>
+        </section>
+    </article>
+</template>
+
+<script setup>
+    import Figure from '@/components/media/Figure.vue';
+    import Repo from '@/components/portfolio/Portfolio.vue';
+</script>
