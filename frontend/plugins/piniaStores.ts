8ddd7c3b39e19b52e7a36a4f2a942b8ca952baad
krigjo25 <krigjo25@gmail.com>
2025-09-18 18:15:38 +0200

    Initializing the Pinia Store Plugin

---
+++ frontend/plugins/piniaStores.ts
@@ -0,0 +1,25 @@
+// Plugins -> Pinia stores
+import { portfolioStore } from '~/stores/portfolioStore'
+//import { academicStore } from '~/stores/academicStore.js'
+//import { achievementsStore } from '~/stores/achievementsStore.js'
+//import { repositoryStore } from '~/stores/repoStore.js'
+//import { academicStore } from '#imports'
+
+
+export default defineNuxtPlugin(async (nuxtApp) =>
+    {
+        
+        const portfolio = portfolioStore();
+        await portfolio.fetchData();
+
+        //const useRepoStore = repositoryStore();
+        //await useRepoStore.fetchData();
+
+        //const useAcademicStore = academicStore();
+        //await useAcademicStore.fetchData();
+
+        
+
+        //const useAchievementsStore = achievementStore();
+        //await useAchievementsStore.fetchData();
+})
