9679d54cb832d992ef6d1618c7ffe8c9d02e1708
krigjo25 <krigjo25@gmail.com>
2025-09-20 15:32:22 +0200

    refactor(timeline) Merge Academic and Achievements components into a single component
        As `Academic` and `Achievements` component's function were identically, leading to unnecessary code duplication and maintance burden. They have been successfully merged into reusable component named `Timline`
    
    * **Consolidation**
        -   The logic and template from both `Academic` and
        `Achievements` have been unified into the new **Timeline.vue** component.
    
    * **Component Removal**
        -   The redundant files `Academic.vue` and `Achievements.vue` has been removed.
    
    * ** File Movement**
        -   The new `Timeline.vue` has been moved to a new logical location from `/components` to `/components/timeline` and associated logic
    
    * **Prop Changes**
    
    BREAKING CHANGE: All files importing or using the old `Academic` and `Achievements` components is already updated to use the new `Timeline` component.

--- frontend/components/timeline/Timeline.vue
+++ frontend/components/timeline/Timeline.vue
@@ -1,41 +1,95 @@
 <template>
-    <section :class="cls[0]">
-        <h2 :class="cls[1]">{{ data.field.title }}</h2>
-        <FormInputs v-if="data.field.rangeMax > 1" :data="data.field" :cls="cls[2]" v-model="modelValue" />
-    </section>
+    <template>
+        <section :class="[cls[0], cls[1]]">
+            <section :class="cls[2], cls[3]">
+                <TimelineFilter
+                :data="range"
+                :cls="[['flex-column-align-items-center', 'timeline-item'],
+                'timeline-input-label', 'timeline-input']"
+                @toggleVisibility="toggleVisibility"/>
+            </section>
+
+            <section :class="cls[3]">
+                <h3>
+                    <DateYear v-for="data in timeline" :key="data.id"
+                        :year="data.year" :isVisible="data.isVisible"/>
+                </h3>
+            </section>
+
+            <section :class="cls[3]">
+                <TimelineCard v-for="data in timeline" :key="data.id"
+                        :data="data" :cls="
+                        [
+                            [
+                                'flex-wrap-column', 
+                                cls[4],
+                                'timeline-content'
+                            ],
+                                'flex-column-justify-center-align-center',
+                'flex-wrap-row-align-content-start-justify-space-evenly',
+                ['tech-container', 'flex-wrap-row-justify-space-evenly'], 'tech-item', 'timeline-description',
+                    'timeline-list', 'timeline-item']"/>
+            </section>
+        </section>
+
+    </template>
+
 </template>
+<script setup lang="ts">
 
-<script setup>
+    interface Props
+    {
+        cls?: Array<any>;
+        range?: Record<string,any>;
+        data: Record<string, any>;
+    }
 
-    import {computed, defineProps, defineEmits, reactive } from 'vue';
+    import { computed } from 'vue'
 
-
-    const props = defineProps({
-        data:
-        {
-            type: Object,
-            required: true
-        },
-        cls:
-        {
-            type: Array,
-            required: false,
-        },
+    const props = withDefaults(defineProps<Props>(),
+    {
+        range: () =>
+                ({
+                    "value": 0,
+                    "rangeMax": 0,
+                    "type": 'range',
+                    "name": "akademic-timeline",
+                    "title": 'Undefined Timeline',
+                    
+                }),
+        cls: () => ['component-blue', 'timeline-container',
+        'timeline-line', 'flex-wrap-row-justify-space-evenly', 'component-w-g-b']
     });
 
-    const data = reactive(props.data);
-    const cls = !!props.cls ? props.cls : null;
-    const emits = defineEmits(['toggleVisibility']);
+    const cls = computed(() => 
+    {
+        const parentCls = Array.isArray(props.cls) ? props.cls : []; 
+        const replacementCls = parentCls.length > 0 ? parentCls[0] : 'component-blue';
+        const defaultCls = ['component-blue', 'timeline-container', 'timeline-line', 'flex-wrap-row-justify-space-evenly'];
 
-    const modelValue = computed(
+        const modDefault = defaultCls.map((c) =>
         {
-            get: () => data.field.value,
-            set: (value) => 
-            {
-                emits('toggleVisibility', value);
+            if (c === 'component-blue' && c != replacementCls) {
+                return replacementCls;
             }
-            
+            return c;
         });
-        //console.warn("Range value changed:", rangeValue);
-    //console.warn("Timeline data:", data.value);
+
+        return [
+            ...modDefault,
+            ...parentCls.slice(1).filter(c => !modDefault.includes(c))
+        ];
+    });
+
+    const range = props.range;
+    const timeline = computed(() => props.data);
+
+
+    const emits = defineEmits(['toggleVisibility'])
+    const toggleVisibility = (id:number) => 
+        {
+            emits('toggleVisibility', id);
+        };
+
+    //console.warn("Academic data on load:", academic.timelines);
 </script>
