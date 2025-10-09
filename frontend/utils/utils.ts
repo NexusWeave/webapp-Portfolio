ac8d4334bb58413f75904c1fbc978f133db36e9b
krigjo25 <krigjo25@gmail.com>
2025-10-09 17:30:04 +0200

    chore(utils/techstack): Remove deprecated code
    Deleted redundant and deprecated code blocks related to previous data
    structure versions within the techstack utility file. This reduces
    technical debt and simplifies the file for future maintenance.

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -1,26 +1,3 @@
-const codeExtensions:(string[]) =
-[
-    //frontend
-    'js', 'html', 'css',
-
-    // Backend
-    'py', 'c', 'cs', 
-    
-    //  Workflow
-    'cms', 'git', 'sass', 'agile'];
-
-export const fetchExtensionType = (element:string): string[] =>
-{
-    element = element.toLowerCase();
-
-    const c = ['.net']
-    if (element in c) element == 'cs' 
-
-    if (!!codeExtensions.filter(ext => element.endsWith(ext))) return codeExtensions.filter(ext => element.endsWith(ext))
-    
-    return []
-}
-
 export function generateHexID()
 {    
     const ch: string = '0123456789abcdef';
