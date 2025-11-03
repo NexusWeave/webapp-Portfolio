54be2b02f2679349a71b41d69e5864b6a3f4d4dd
krigjo25 <krigjo25@gmail.com>
2025-11-03 15:21:05 +0100

    feat(utils): Initialize array sequence command and timer utility     -   Added two essential, universal utility functions to `utils.ts`     to handle array iteration and timing events.
    * **Array Navigation**
        -   Implemented a universal function (`next`) to
        correctly determine and display the **next index** in an array,
        enabling reliable sequence control (e.g., for carousels or other array based components).
    
    * **Timer**
        -   Initialized a generic **timer utility** function to manage
        time-based operations within components.

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -8,3 +8,40 @@ export function generateHexID()
 
     return result;
 }
+
+export function next(arr: Array<Record<string, any>>)
+    {
+
+        const n = arr.length;
+        if (n <= 1) return;
+
+        // Iterate through the array
+        for (let i = 0; i < n; i++)
+        {
+            const length = n - 1;
+            const source = arr[i];
+
+            const j = i == length ? 0 : i + 1;
+            const target = arr[j];
+
+            //  Swap the visibility
+            if (source.isVisible)
+            {
+                const temp: boolean = source.isVisible;
+                source.isVisible = target.isVisible;
+                target.isVisible = temp;
+                break;
+            }
+
+        }
+        //console.log(arr);
+    };
+
+export function startTimer(arr: Array<Record<string, any>>)
+    {
+        //  Set the interval
+        const n = 5000;
+
+        //  Start the timer
+        setInterval(() => next(arr), n);
+    };
