210d0cdf004ec4506745e4f729d879a94c6241ec
krigjo25 <krigjo25@gmail.com>
2025-11-22 13:00:25 +0100

    refactor(announcements): 🔨 Migrated Flask Model to Pydantic Model -   Migrated the Announcements model definition and associated logic to comply with the new FastAPI data standards Pydantic.
    * **Migration**
        -   Rewrote the model structure for Announcements to be compatible
        with the FastAPI framework.
    * **Architecture**
        -   This step continues the deprecation of Flask components and
        solidifies the FastAPI structure.

---
+++ fastAPI/lib/models/announcements.py
@@ -0,0 +1,30 @@
+
+#   Standard libraries
+import __future__
+from typing import Dict
+from datetime import datetime
+
+#   Third Party Libraries
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+#   Custom Libraries
+from lib.utils.utilitytools import UtilityTools
+
+
+#   Load environment variables
+load_dotenv()
+
+class Announcements(BaseModel):
+
+    #   Initialize methods and database
+    announcement_id: int = Field(..., description="Unique Announcement ID", example=1)                          #type: ignore
+    date: datetime = Field(..., description="Announcement Date", example=datetime.now())                        #type: ignore
+    message: str = Field(..., description="Announcement Message", example="New features added to the API.")     #type: ignore
+
+    class Config:
+        """Configuration for the Announcements model."""
+        schema_extra: Dict[str, int | Dict[str, object] | str] = {
+            "example": { "announcement_id": 1, "date": datetime.now(), "message": "New features added to the API." }
+        }
+
