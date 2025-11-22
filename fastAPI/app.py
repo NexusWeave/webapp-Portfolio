ece37b2723bfe131d2961bcdb9963d9ba327cfde
krigjo25 <krigjo25@gmail.com>
2025-11-22 10:07:58 +0100

    refactor(app.py): 🔨 Forbedret organisering og robusthet i hovedfil     -   Organiserte imports etter beste praksis og økte robustheten ved     å legge til feilbehandling for app-konfigurasjoner.
    - Organiserte imports etter beste praksis.
    - Oppdaterte Logger-navnet.
    - La til en try-except for å hente app-konfigurasjoner.
    - Fjernet hardkoding for tittel og versjon.

--- fastAPI/app.py
+++ fastAPI/app.py
@@ -1,49 +1,51 @@
-#  Application entry point
-
-#   Importing required depenedencies
+#   Standard Depnendencies
 import os
+
+
+#   Third Party Dependencies
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 
-from dotenv import load_dotenv
-
-
-#   Custom dependencies
+#   Local Dependencies
+from lib.utils.app_utility import AppTools
 from lib.utils.logger_config import AppWatcher
-
-#   Endpoint services
 #   from lib.endpoint_services.github_data import Github
 #   from lib.endpoint_services.Photos import PhotoLibrary
 #   from lib.endpoint_services.announcements import Announcements
-from lib.settings.env_config import DevelopmentConfig, ProdConfig
+
 
 #   Initialize Enviorment variables
 load_dotenv()
 
 # Initialize the logger
-logger = AppWatcher(dir="logs", name='Flask-App')
+logger = AppWatcher(dir="logs", name='FastAPI-App')
 logger.file_handler()
 
-#   Initialize Flask app and Extensions
-app = FastAPI(title="Portfolio Backend API", version="1.0.0")
+try:
+    config = AppTools.setup_environment(os.getenv('ENV', 'development'))
 
-match str(os.getenv('ENV')).lower():
-    case 'production':
-        app.add_middleware(CORSMiddleware,ProdConfig.CORS_ORIGINS, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-        logger.info("Loading the Production Environment")                                                                                     #type: ignore
+except ValueError as ve:
+    logger.error(f"Error in setting up the environment: {ve}")
+    raise ve
 
-    case 'development':
-        app.add_middleware(CORSMiddleware,DevelopmentConfig.CORS_ORIGINS, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-        logger.info("Loading the Development Environment")                                                                                     #type: ignore
+logger.info(f"Configuration for \'{config.API_NAME}\' - \'v{config.API_VERSION}\' loaded \'{config.ENVIRONMENT}\'- Environment successfully.") 
+    
+#   Initialize FastAPI
+app = FastAPI(title = config.API_NAME, version=config.API_VERSION)
 
-    case _:
-        logger.error("Invalid environment variable. Set ENV to either 'production' or 'development'.")                                          #type: ignore
-        raise ValueError("Invalid environment variable. Set ENV to either 'production' or 'development'.")
-
-#   Registering Enpoint Services
-#   @app.get("/api/v2/blogs/heavy/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config.CORS_ORIGINS, 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Portfolio Backend API"}
+#   Registering Enpoint Services
+#   @app.get("/api/v2/blogs/heavy/")
+
 # @app.get("/api/v2/announcement", tags=["Announcements"])
 # @app.get("/api/v2/portfolio/github/{username}", tags=["GitHub"])
