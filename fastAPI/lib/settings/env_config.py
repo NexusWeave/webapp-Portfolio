962a35aa790bbc914349118e0bd813514f8038cd
krigjo25 <krigjo25@gmail.com>
2025-11-22 09:14:55 +0100

    refactor(env): 🔨 Omstrukturerte og oppdaterte FastAPI- env-config.     -   Gjennomførte en refaktorering av innstillingsmodulen for å forbedre     organiseringen og funksjonaliteten til FastAPI-konfigurasjonen.
    * **Omstrukturering**
        -   Omorganiserte og forenklet strukturen i konfigurasjonsfilene/klassene.
    
    * **Oppdatering**
        -   Oppdaterte innstillingene for å reflektere de siste kravene for
        FastAPI-miljøet.

--- fastAPI/lib/settings/env_config.py
+++ fastAPI/lib/settings/env_config.py
@@ -3,25 +3,34 @@
 # Importing required modules
 import os
 from typing import List
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+#   Initialize Enviorment variables
+load_dotenv()
 
 class Config(BaseSettings):
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8") #type: ignore
-
     DEBUG: bool = False
-    ENVIRONMENT: str | None
     SECRET_KEY: str | None
     CORS_ORIGINS: List[str]
+    ENVIRONMENT: str
     DATABASE_URL: str | None
+    API_VERSION: str = "1.0.0"
+    API_NAME: str = "Portfolio Backend API"
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 class ProdConfig(Config):
-    ENVIRONMENT: str | None = 'production'
-    CORS_ORIGINS: List[str] = os.getenv('CORS_ORIGINS') if (os.getenv('CORS_ORIGINS')) else [] #type: ignore
 
-    
+    ENVIRONMENT: str = 'production'
+    SECRET_KEY: str | None = os.getenv('SECRET_KEY', 'changeme')
+    DATABASE_URL: str | None = os.getenv('DATABASE_URL', 'sqlite:///./prod.db')
+    CORS_ORIGINS: List[str] = os.getenv('CORS_ORIGINS', f'{['localhost:3000', 'http://localhost:3000', 'https://localhost:3000']}').split(',')
 
 class DevelopmentConfig(Config):
+
     DEBUG: bool = True
-    ENVIRONMENT: str | None = 'development'
+    ENVIRONMENT: str = 'development'
+    SECRET_KEY: str | None = os.getenv('SECRET_KEY', 'changeme')
+    DATABASE_URL: str | None = os.getenv('DATABASE_URL', 'sqlite:///./prod.db')
     CORS_ORIGINS: List[str] = ['localhost:3000', 'http://localhost:3000', 'https://localhost:3000']
