2b82d03fef253c9bccb657241cd39d7faf9f82fa
krigjo25 <krigjo25@gmail.com>
2025-12-05 15:57:06 +0100

    chore(Database): 🗃️ Introducing a missing files required for database feature.

---
+++ fastAPI/lib/services/database/resources.py
@@ -0,0 +1,25 @@
+#   Standard Libraries
+import os
+
+#   Third-Party Libraries
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+#   Local Libraries
+from lib.services.database_services import Sqlite3
+
+load_dotenv()
+
+#   Database Configuration
+SQLITE3_URL = os.getenv("SQLITE3_URL", "sqlite+aiosqlite:///./test.db")
+SQLITE3_Engine = create_async_engine( SQLITE3_URL, echo=True, future=True)
+SQLITE3_SessionLocal = async_sessionmaker(
+    class_=AsyncSession,
+    bind=SQLITE3_Engine,
+    expire_on_commit=False,
+)
+
+SQLITE_INSTANCE = Sqlite3(
+    engine=SQLITE3_Engine,
+    session_factory=SQLITE3_SessionLocal
+)
