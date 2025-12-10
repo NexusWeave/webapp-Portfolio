8fa1b43de3039564739cc57bbbaf7f2bf7520ca0
krigjo25 <krigjo25@gmail.com>
2025-12-10 14:09:36 +0100

    refactor(Database): 🗃️ Eliminated Hardcoding for Database Configurations.

--- fastAPI/lib/services/database/resources.py
+++ fastAPI/lib/services/database/resources.py
@@ -4,16 +4,22 @@ import os
 #   Third-Party Libraries
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from lib.models.database_models.GithubModel import RepositoryModel, LanguageModel, CollaboratorModel, LanugageRepositoryAssosiationModel #type: ignore
 
 #   Local Libraries
 from .databases import Sqlite3
 
 load_dotenv()
+SEPERATOR: str = ":///"
+DIALECT: str = os.getenv("SQLITE3_DIALECT", "sqlite")
+DRIVER: str = os.getenv("SQLITE3_DRIVER", "aiosqlite")
+
+PATH : str = os.getenv("SQLITE3_PATH", "./databases/sqliteDatabase.db")
 
 #   Database Configuration
-SQLITE3_URL = os.getenv("SQLITE3_URL", "sqlite+aiosqlite:///./test.db")
+SQLITE3_URL = os.getenv("SQLITE3_URL", f"{DIALECT}+{DRIVER}{SEPERATOR}{PATH}")
 SQLITE3_Engine = create_async_engine( SQLITE3_URL, echo=True, future=True)
-SQLITE3_SessionLocal = async_sessionmaker(
+SQLITE3_SESSIONLOCAL = async_sessionmaker(
     class_=AsyncSession,
     bind=SQLITE3_Engine,
     expire_on_commit=False,
@@ -21,5 +27,5 @@ SQLITE3_SessionLocal = async_sessionmaker(
 
 SQLITE_INSTANCE = Sqlite3(
     engine=SQLITE3_Engine,
-    session_factory=SQLITE3_SessionLocal
+    session_factory=SQLITE3_SESSIONLOCAL
 )
