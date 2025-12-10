8e539eb19bdf9833edfd2c226b5a5a40f9ebdc32
krigjo25 <krigjo25@gmail.com>
2025-12-10 13:39:30 +0100

    refactor(Announcements): ♻️ Renaming get_holidays -> get_celebration_days

--- fastAPI/lib/services/announcements.py
+++ fastAPI/lib/services/announcements.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class AnnouncementsService(object):
 
     @staticmethod
-    def get_holidays(date: datetime) -> str | None:
+    def get_celebration_days(date: datetime) -> str | None:
         match (date):
 
             case _ if date.month == 2 and date.day > 9 and date.day < 15:
@@ -17,13 +17,13 @@ class AnnouncementsService(object):
             case _ if date.month == 5 and date.day < 18 and date.month == 5 and date.day > 9:
                 message ="🇳🇴 Happy Independence Day Norway 🇳🇴"
 
-            case _ if date.month == 10 and date.day > 20 and date.month == 11 and date.day < 1:     #type: ignore
+            case _ if date.month > 9 and date.day > 20 and date.month < 12 and date.day < 1:
                 message ="👻 Happy Halloween 👻"
 
             case _ if date.month == 12 and date.day > 10 and date.day < 26:
                 message ="🎅 Merry Christmas ! 🎅"
 
-            case _ if date.month == 12 and date.day >= 30 and date.month == 1 and date.day < 10:     #type: ignore
+            case _ if date.month > 11 and date.day >= 30 and date.month  < 2 and date.day < 10:
                 message ="🎉 Happy New Year 🎉"
 
             case _: 
