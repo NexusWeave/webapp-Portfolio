73b8420d0e7c4b902fb706406bfe1277449c6de3
krigjo25 <krigjo25@gmail.com>
2025-12-15 14:09:40 +0100

    refactor(Model): ♻️ Restructure GithubModel Dependencies, Relationships and naming Conventions.     -   Performed refactoring on the GithubModel and related data structures     to enhance clarity, maintainability, and structural coherence.
    * **Dependency Organization**
        -   Reorganized the import structure for third-party libraries and
        renamed local libraries to "Internal Dependencies" for clearer modular
        separation.
    
    * **Model Naming & Cleanup**
        -   Renamed the `LanguageRepositoryAssosiationModel`` to the more
        concise LanguageAssosiationModel and removed the unnecessary
        CollaboratorsModel.
    
    * **Variable & Relationship Adjustments**
        -   Renamed various internal variables for better semantic clarity
        and updated underlying model relationships to align with the refined
        data structure.

--- fastAPI/lib/models/database_models/GithubModel.py
+++ fastAPI/lib/models/database_models/GithubModel.py
@@ -1,8 +1,9 @@
-#   Third-Party Libraries
-from sqlalchemy import Column, String, Integer, ForeignKey
+#   Third-Party Dependencies
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, ForeignKey
 
-#   Local Libraries
+
+#   Internal Dependencies
 from lib.services.base_services.database_config import BASE
 
 class RepositoryModel(BASE):
@@ -27,11 +28,10 @@ class RepositoryModel(BASE):
     created_at = Column(String, nullable=False)
 
     #   Relationships
-    collaborators = relationship("CollaboratorModel", back_populates="repository", cascade="all, delete-orphan")
-    assosiations = relationship("LanugageRepositoryAssosiationModel", back_populates="repository", cascade="all, delete-orphan")
+    #collab_assosiations = relationship("CollaboratorModel", back_populates="repository", cascade="all, delete-orphan")
+    lang_assosiations = relationship("LanguageAssosiationModel", back_populates="repository", cascade="all, delete-orphan")
 
-
-class LanugageRepositoryAssosiationModel(BASE):
+class LanguageAssosiationModel(BASE):
 
     __tablename__: str = "language_repository_association"
 
@@ -41,27 +41,15 @@ class LanugageRepositoryAssosiationModel(BASE):
     code_bytes = Column(Integer, nullable=False)
 
     #   Relationships
-    language = relationship("LanguageModel", back_populates="assosiations")
-    repository = relationship("RepositoryModel", back_populates="assosiations")
+    language = relationship("LanguageModel", back_populates="lang_assosiations")
+    repository = relationship("RepositoryModel", back_populates="lang_assosiations")
     
 class LanguageModel(BASE):
 
     __tablename__: str = "languages"
 
     id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
-    language = Column(String, unique=True, index=True, nullable=False)
+    lang = Column(String, unique=True, index=True, nullable=False)
 
     #   Relationships
-    assosiations = relationship("LanugageRepositoryAssosiationModel", back_populates="language")
-
-class CollaboratorModel(BASE):
-
-    __tablename__: str = "collaborators"
-
-    name = Column(String, unique=True, index=True, nullable=False)
-    collab_id = Column(String, unique=True, index=True, nullable=False)
-    repo_id = Column(Integer, ForeignKey('repositories.id'), nullable=False)
-    id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
-
-    #   Foreign Keys
-    repository = relationship("RepositoryModel", back_populates="collaborators")
+    lang_assosiations = relationship("LanguageAssosiationModel", back_populates="language")
