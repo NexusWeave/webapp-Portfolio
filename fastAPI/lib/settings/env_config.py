4ee654ff7ef8c569516c5a13df861a10a50ca73f
krigjo25 <krigjo25@gmail.com>
2025-12-19 15:41:23 +0100

    refactor(Types): ♻️ Forandret type-hints fra 'str | None' til 'Optional'     -   Standardiserer bruken av Optional[str] i stedet for pipe-syntaks     Øke kode konsistensen. Sikrer at type-hinting følger prosjektets     etablerte mønster for validering og statisk analyse.

--- fastAPI/lib/settings/env_config.py
+++ fastAPI/lib/settings/env_config.py
@@ -4,7 +4,7 @@
 import __future__, os
 
 # Third Party Libraries
-from typing import List
+from typing import List, Optional
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -15,22 +15,25 @@ load_dotenv()
 class Config(BaseSettings):
     ENVIRONMENT: str
     DEBUG: bool = False
-    SECRET_KEY: str | None
+    SECRET_KEY: Optional[str]
     API_VERSION: str = "v1"
     CORS_ORIGINS: List[str]
-    DATABASE_URL: str | None
+    DATABASE_URL: Optional[str]
+    DATABASE_TOKEN: Optional[str]
     API_NAME: str = "Portfolio Backend API"
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 class ProdConfig(Config):
     ENVIRONMENT: str = 'production'
-    SECRET_KEY: str | None = os.getenv('SECRET_KEY', None)
-    DATABASE_URL: str | None = os.getenv('PROD_DATABASE_URL', None)
+    SECRET_KEY: Optional[str] = os.getenv('SECRET_KEY', None)
+    DATABASE_URL: Optional[str] = os.getenv('PROD_DATABASE', None)
+    DATABASE_TOKEN: Optional[str] = os.getenv('TURSO_WRITE_TOKEN', None)
     CORS_ORIGINS: List[str] = os.getenv('CORS_ORIGINS', f'{['localhost:3000', 'http://localhost:3000', 'https://localhost:3000']}').split(',')
 
 class DevelopmentConfig(Config):
     DEBUG: bool = True
     ENVIRONMENT: str = 'development'
-    SECRET_KEY: str | None = os.getenv('SECRET_KEY', None)
-    DATABASE_URL: str | None = os.getenv('DEV_DATABASE_URL', None)
+    SECRET_KEY: Optional[str] = os.getenv('SECRET_KEY', None)
+    DATABASE_URL: Optional[str] = os.getenv('DEV_DATABASE', None)
+    DATABASE_TOKEN: Optional[str] = os.getenv('DEV_DATABASE_TOKEN', None)
     CORS_ORIGINS: List[str] = ['localhost:3000', 'http://localhost:3000', 'https://localhost:3000']
