23df74b9545a460538784d1464b98d191ed33dbe
krigjo25 <krigjo25@gmail.com>
2025-12-22 14:37:07 +0100

    refactor(Model): ♻️ Added Frontend Mapping functions to GithubModel     -   Bridge the gap between raw data and UI representation. Initialized     UI-specific transformation logic into the model layer, this keeps the     responsebility for mapping at the backend.
    *   **Anchor Mapping**
        -   Implemented logic to map GitHub data to the frontend Anchor component requirements.
    
    *   **Icon Mapping**
        -   Added functions to resolve icons based on repository data/languages for visual consistency.

--- fastAPI/lib/models/github_model.py
+++ fastAPI/lib/models/github_model.py
@@ -1,5 +1,5 @@
 #   Standard Dependencies
-import __future__
+import __future__, uuid
 from datetime import datetime
 from typing import Dict, List, Optional
 
@@ -7,11 +7,18 @@ from typing import Dict, List, Optional
 from pydantic import BaseModel, Field, ConfigDict, computed_field
 
 
+class LanguageImage(BaseModel):
+    id : int
+    alt: str
+    src: str
+    type: str = "svg"
+
 class LanguageModel(BaseModel):
     id: int = Field(..., description = "Language ID", json_schema_extra = {"example":1})
     language: str = Field(..., alias = "lang", description = "Language Name", json_schema_extra = {"example":"Python"})
 
     model_config = ConfigDict(from_attributes = True)
+
 
 class LanguageAssociationModel(BaseModel):
     lang_id: int = Field(..., description = "Language ID", json_schema_extra = {"example":1})
@@ -29,24 +36,91 @@ class GithubModel(BaseModel):
     label: str = Field(..., description = "Repository Name", json_schema_extra = {"example":"my-repo"})
     owner: str = Field(..., description = "Repository Owner", json_schema_extra = {"example":"username"})
     created_at: datetime = Field(..., description = "Creation Timestamp", json_schema_extra = {"example":f"{datetime.now()}"})
+    id: int = Field(..., alias = "repo_id", description = "Unique Repository ID", json_schema_extra = {"example":"1234567890"})
     demo_url: Optional[str] = Field(None, description = "Demo URL", json_schema_extra = {"example":"https://demo.krigjo25.com"})    
-    repo_id: int = Field(..., description = "Unique Repository ID", json_schema_extra = {"example":"1234567890"})
-    description: Optional[str] = Field(None, description = "Repository Description", json_schema_extra = {"example":"This is my repository."})
     repo_url: str = Field(..., description = "Repository URL", json_schema_extra = {"example":"https://github.com/username/my-repo"})
     youtube_url: Optional[str] = Field(None, description = "YouTube URL", json_schema_extra = {"example":"https://youtube.com/my-repo"})
+    description: Optional[str] = Field(None, description = "Repository Description", json_schema_extra = {"example":"This is my repository."})
 
     lang_assosiations: List[LanguageAssociationModel] = Field(..., exclude= True)
 
     @computed_field
-    @property
-    def languages(self) -> List[Dict[str, str | int]]:
+    def languages(self) -> List[Dict[str, str | int | Dict[str, str] | object]]:
         
-        languages: List[Dict[str, str | int]] = []
+        languages: List[Dict[str, str | int | Dict[str, str] | object]]= []
 
         for assec in self.lang_assosiations:
             
-            if assec.language.id == assec.lang_id and self.repo_id == assec.repo_id:
-                languages.append({"language": assec.language.language, "bytes": assec.code_bytes})
+            if assec.language.id == assec.lang_id and self.id == assec.repo_id:
+                languages.append(
+                    {
+                        "bytes": assec.code_bytes,
+                        "label": assec.language.language, 
+                        "image": {
+                            "type":'svg',
+                            "alt": f'Logo for {assec.language.language}',
+                            "src": f'/public/media/tech-language/{assec.language.language}.svg'
+                        }
+                        })
+        
         return languages
     
+    @computed_field
+    def anchor(self) -> List[Dict[str, str | object]]:
+
+        ANCHOR: List[Dict[str, str | object]] = [
+            {
+                'name': 'github',
+                'id': uuid.uuid4().hex,
+                'href': self.repo_url,
+                'type': ['github','external']
+            },
+            {
+                'name': 'ytube',
+                'id': uuid.uuid4().hex,
+                'href': self.youtube_url,
+                'type': ['youtube','external']
+            },
+            {
+                'name': 'demo',
+                'id': uuid.uuid4().hex,
+                'href': self.demo_url,
+                'type': ['demo','external']
+            }
+        ]
+
+        return ANCHOR
+
+    @computed_field
+    def icon(self) -> List[Dict[str, str]]:
+        
+        languages: List[Dict[str, str]] = []
+
+        for assec in self.lang_assosiations:
+            
+            if assec.language.id == assec.lang_id and self.id == assec.repo_id:
+                languages.append(
+                    {
+                        "type": "svg",
+                        "id"  : f"{self.id}",
+                        "alt" : f'Logo for {assec.language.language}',
+                        "src" : f'/media/tech-lang-icons/{assec.language.language}.svg'
+                    })
+        
+        return languages
+
+    @computed_field
+    def name(self) -> List[str]:
+        sep = '-'
+        label: List[str] = str(self.label).split(sep)
+        return label
+
+    @computed_field
+    def date(self) -> Dict[str, str]:
+        date: Dict[str, str] = {
+            "created": self.created_at.strftime("%d-%m-%Y")
+        }
+        return date
+
+
     model_config = ConfigDict(from_attributes=True)
