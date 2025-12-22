fca301c64e26cd22cd56d4b1efea6697bb8ece5b
krigjo25 <krigjo25@gmail.com>
2025-12-22 11:26:12 +0100

    feat(Config): ✨ Integrate Environment Strings into ConfigurationClasses

--- fastAPI/lib/settings/env_config.py
+++ fastAPI/lib/settings/env_config.py
@@ -21,19 +21,35 @@ class Config(BaseSettings):
     DATABASE_URL: Optional[str]
     DATABASE_TOKEN: Optional[str]
     API_NAME: str = "Portfolio Backend API"
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    #   Enviorment strings
+    GithubToken:str
+    GithubBase: str
+    GithubRepos: str
+    
+    HEAVYV: str
+    HEAVYWORKOUTS:str
+    HEAVYCOUNT: str
+    HEAVYAPI: str
+    HEAVYTOKEN: str
+
+    ENV: str
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 class ProdConfig(Config):
-    ENVIRONMENT: str = 'production'
+    ENVIRONMENT: str =  os.getenv('ENV', 'production')
     SECRET_KEY: Optional[str] = os.getenv('SECRET_KEY', None)
     DATABASE_URL: Optional[str] = os.getenv('PROD_DATABASE', None)
     DATABASE_TOKEN: Optional[str] = os.getenv('TURSO_WRITE_TOKEN', None)
     CORS_ORIGINS: List[str] = os.getenv('CORS_ORIGINS', f'{['localhost:3000', 'http://localhost:3000', 'https://localhost:3000']}').split(',')
 
+
 class DevelopmentConfig(Config):
     DEBUG: bool = True
-    ENVIRONMENT: str = 'development'
+    ENVIRONMENT: str =  os.getenv('ENV', 'development')
     SECRET_KEY: Optional[str] = os.getenv('SECRET_KEY', None)
     DATABASE_URL: Optional[str] = os.getenv('DEV_DATABASE', None)
     DATABASE_TOKEN: Optional[str] = os.getenv('DEV_DATABASE_TOKEN', None)
     CORS_ORIGINS: List[str] = ['localhost:3000', 'http://localhost:3000', 'https://localhost:3000']
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
