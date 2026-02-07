ac36f55337aa2666cc5db8ec6cf2e19dc6709469
krigjo25 <krigjo25@gmail.com>
2026-02-07 09:46:49 +0100

    refactor(Database): 🚚 Encapsulating DB Context.

--- fastAPI/lib/database/databases.py
+++ fastAPI/lib/database/databases.py
@@ -9,14 +9,14 @@ from lib.settings.database_config import DatabaseConfig
 LOG = DatabaseWatcher(dir="logs", name="DatabaseService")
 LOG.file_handler()
 
-class Sqlite3(DatabaseConfig):
+class Sqlite3Provider(DatabaseConfig):
 
     def __init__(self, engine: Engine, session_factory: sessionmaker[Session]):
         super().__init__(engine, session_factory)
         LOG.info("Sqlite3 Service Initialized")
 
 
-class Postgres(DatabaseConfig):
+class PostgressProvider(DatabaseConfig):
 
     def __init__(self, engine: Engine, session_factory: sessionmaker[Session]):
         super().__init__(engine, session_factory)
--- fastAPI/lib/database/db_engine.py
+++ fastAPI/lib/database/db_engine.py
@@ -6,26 +6,35 @@ from typing import Optional
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from lib.models.database_models.GithubModel import RepositoryModel, LanguageModel, LanguageAssosiationModel #type: ignore
 
 #   Internal Dependencies
-from .databases import Sqlite3
+from databases import Sqlite3Provider, PostgressProvider
 
 load_dotenv()
 
-TURSO_DB: Optional[str] = os.getenv('PROD_DATABASE', None)
-PATH : str = f"sqlite+{TURSO_DB}?secure=true"
-TURSO_TOKEN : Optional[str] = os.getenv('TURSO_WRITE_TOKEN', "local.db")
+def initialize_sqlite_engine() -> Sqlite3Provider:
+    SQLITE3_DB: Optional[str] = os.getenv('SQLITE3_TOKEN', None)
+    SQLITE3_TOKEN : Optional[str] = os.getenv('SQLITE3_TOKEN', "local.db")
 
-#   Database Configuration
-SQLITE3_Engine = create_engine( PATH, connect_args={"auth_token": TURSO_TOKEN})
-SQLITE3_SESSIONLOCAL = sessionmaker(
-    class_=Session,
-    bind=SQLITE3_Engine,
-    expire_on_commit=False,
-)
+    #   Database Configuration
+    PATH : str = f"sqlite+{SQLITE3_DB}?secure=true"
+    SQLITE3_Engine = create_engine( PATH, connect_args={"auth_token": SQLITE3_TOKEN})
+    SQLITE3_SESSIONLOCAL = sessionmaker( class_ = Session, bind = SQLITE3_Engine, expire_on_commit = False)
 
-SQLITE_INSTANCE = Sqlite3(
-    engine=SQLITE3_Engine,
-    session_factory=SQLITE3_SESSIONLOCAL
-)
+    return Sqlite3Provider( engine = SQLITE3_Engine, session_factory = SQLITE3_SESSIONLOCAL)
+
+def initialize_postgress_engine() -> PostgressProvider:
+    POSTGRESS_DB: Optional[str] = os.getenv('POSTGRESS_DATABASE', None)
+    POSTGRESS_TOKEN : Optional[str] = os.getenv('POSTGRESS_TOKEN', "local.db")
+
+    #   Database Configuration
+    PATH : str = f"sqlite+{POSTGRESS_DB}?secure=true"
+    POSTGRESS_ENGINE = create_engine( PATH, echo=False, pool_pre_ping=True,
+        connect_args={"sslmode": "require"} if "neon.tech" in DATABASE_URL else {}
+    )
+    SQLITE3_SESSIONLOCAL = sessionmaker( class_ = Session, bind = POSTGRESS_ENGINE, expire_on_commit = False)
+
+    return PostgressProvider( engine = POSTGRESS_ENGINE, session_factory = SQLITE3_SESSIONLOCAL)
+
+SQLITE_INSTANCE = initialize_sqlite_engine()
+POSTGRES_INSTANCE = initialize_postgress_engine()
