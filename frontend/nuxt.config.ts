dd47bde4d1434cdb0d3b410e953616be796e8410
krigjo25 <krigjo25@gmail.com>
2026-02-11 18:24:21 +0100

    chore(Env): 🔧 Configuring environments.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,33 +6,13 @@ const srcDir = dirname(fileURLToPath(import.meta.url)); // Du har denne allerede
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  dir: { public:'public' },
+  vite: { resolve: { alias: {'$src': `${srcDir}`,} } },
+  css: [ `~/sass/index.sass`, 'bootstrap-icons/font/bootstrap-icons.css' ],
+  modules: [ '@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@pinia/nuxt' ],
   runtimeConfig:{
     public:{
-      gcloud_api: process.env.GCLOUD || 'http://127.0.0.1:8000/',
+      GCLOUD: process.env.GOOGLE_CLOUD || 'http://0.0.0.0:8080',
     }
-  },
-  modules: [
-    '@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@nuxt/ui',
-    '@pinia/nuxt'
-  ],
-  css:
-  [
-    `~/sass/index.sass`,
-    'bootstrap-icons/font/bootstrap-icons.css'
-  ],
-  vite:
-  {
-    resolve:
-    {
-      alias:
-      {
-        '$src': `${srcDir}`,
-        }
-      }
-  },
-  dir:
-  {
-    public:'public'
   }
 })
