21ff6f937495943c42a8e43421fb4092168f732d
krigjo25 <krigjo25@gmail.com>
2026-03-11 15:12:43 +0100

    fix(LOG): 🐛 Resolved some Type Issues, by removing level argument & os.

--- fastAPI/lib/utils/logger_config.py
+++ fastAPI/lib/utils/logger_config.py
@@ -1,6 +1,5 @@
 #   Python Libraries
 from __future__ import annotations
-import os
 from typing import Optional
 
 #   Third Party Libraries
@@ -9,20 +8,20 @@ from std_log import Logger
 
 class AppWatcher(Logger):
     def __init__(self, name: str, dir:Optional[str] = None, level: int = 0):
-        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log", level=level)
+        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log")
 
 class APIWatcher(Logger):
     def __init__(self, name:Optional[str] = None, dir:Optional[str] = None, level: int = 0):
-        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log", level=level)
+        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log")
 
 class DatabaseWatcher(Logger):
     def __init__(self, name:Optional[str] = None, dir:Optional[str] = None, level: int = 0):
-        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log", level=level)
+        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log")
 
 class ServiceWatcher(Logger):
     def __init__(self, name:Optional[str] = None, dir:Optional[str] = None, level: int = 0):
-        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log", level=level)
+        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log")
 
 class ConfigWatcher(Logger):
     def __init__(self, name:Optional[str] = None, dir:Optional[str] = None, level: int = 0):
-        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log", level=level)
+        super().__init__(dir = dir or '.logs', name=f"{self.__class__.__name__} -- {name}.log")
