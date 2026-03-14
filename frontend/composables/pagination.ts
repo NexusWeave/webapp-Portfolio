08f5966827ef065109fc2911ca65c99d1c82a1e4
krigjo25 <krigjo25@gmail.com>
2026-03-14 09:07:26 +0100

    fix(Composeable): 🐛 Resolve Type Errors by Unifying Collection References.     -   Replace `BlogCollectionItem` with `DevPostsCollection` for correct     typing.
        -   Remove redundant computed properties to simplify component logic
        and cleaned up assosiated imports.

--- frontend/composables/pagination.ts
+++ frontend/composables/pagination.ts
@@ -1,17 +1,14 @@
-import { computed } from 'vue';
 import { mapBlogData } from './maps/blogPost';
 
-import type { BlogCollectionItem } from '@nuxt/content';
+import type { DevPostsCollectionItem } from '@nuxt/content';
 
-export const blogPagination =  (rawData:BlogCollectionItem[], currentPage:number) =>
+export const blogPagination =  (rawData:DevPostsCollectionItem[], currentPage:number) =>
     {
-        return computed(() => {
-            if (!rawData) return [];
+        if (!rawData) return [];
         const n = 3;
         const data = mapBlogData(rawData);
 
         const start = (currentPage - 1) * n;
         const end = start + n;
         return !!data ? data.slice(start, end) : null;
-    });
-}
+    }
