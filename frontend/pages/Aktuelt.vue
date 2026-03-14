6d8ac1022f925ae34623602d20f568610a732698
krigjo25 <krigjo25@gmail.com>
2026-03-14 10:55:57 +0100

    refactor: Add pagnition for Articles.

--- frontend/pages/Aktuelt.vue
+++ frontend/pages/Aktuelt.vue
@@ -1,27 +1,21 @@
 <template>
     <article class="article-wrapper flex-column">
         <h2> Mine Faglige Logger </h2>
-        <section class="blog-section flex-wrap-row">
-            <UtilsPagination v-if="totalDevPages > 1"
-                    :activePage="currentDevPage" 
-                    :totalPage="totalDevPages" 
-                    @update="currentDevPage = $event"
-                />
-            <section v-for="(post, index) in mappedDev" :key="index" class="blog-content">
+        <section class="blog-section flex-wrap-row-justify-space-between">
+            <NavigationButton v-if="currentDevPage > 1" :data="devPageButtons[0]"/>
+            <section v-for="post in mappedDev" :key="post.id" class="blog-content">
                 <ArticleHead :article="post" />
             </section>
+            <NavigationButton v-if="currentDevPage < totalDevPages" :data="devPageButtons[1]"/>
         </section>
 
         <h2> Mine Personlige Logger </h2>
-        <section class="blog-section flex-wrap-row">
-            <UtilsPagination v-if="totalPersonalPages > 1"
-                    :activePage="currentPersonalPage" 
-                    :totalPage="totalPersonalPages" 
-                    @update="currentPersonalPage = $event"
-                />
-            <section v-for="(post, index) in mappedPersonal" :key="index" class="blog-content">
+        <section class="blog-section flex-wrap-row-justify-space-between">
+            <NavigationButton v-if="currentDevPage > 1" :data="devPageButtons[0]"/>
+            <section v-for="post in mappedPersonal" :key="post.id" class="blog-content">
                 <ArticleHead :article="post" />
             </section>
+            <NavigationButton v-if="currentDevPage < totalDevPages" :data="devPageButtons[1]"/>
         </section>
     </article>
 </template>
@@ -44,15 +38,23 @@
     const rawPersonal = await fetchCollection<DevPostsCollectionItem>(personalPath, personalCache);
 
      //  --- Pagination Logic
-    const n = 8;
+    const n = 3;
     const currentDevPage = ref<number>(1);
-    const currentPersonalPage = ref<number>(1);
-
     const totalDevPages = computed(() => { if (rawDev.value) return Math.ceil(rawDev.value.length / n); return 0; });
+    const devPageButtons = computed(() =>
+    [
+        { id: 0, label: 'Forrige', cls: ['button', 'pagination-btn'], action: () => currentDevPage.value -- },
+        { id: 1, label: 'Neste', cls: ['button', 'pagination-btn'], action: () => currentDevPage.value ++ }
+    ]);
+        
+
+    
+    const currentPersonalPage = ref<number>(1);
     const totalPersonalPages = computed(() => { if (rawPersonal.value) return Math.ceil(rawPersonal.value.length / n); return 0; });
 
-    const mappedDev =  computed(() => blogPagination(rawDev.value, currentDevPage.value));
-    const mappedPersonal = computed(() => blogPagination(rawPersonal.value, currentPersonalPage.value));
+
+    const mappedDev =  computed(() => {currentDevPage.value; return blogPagination(rawDev.value, currentDevPage.value)});
+    const mappedPersonal = computed(() => {currentPersonalPage.value; return blogPagination(rawPersonal.value, currentPersonalPage.value)});
 
     // Debugging logs
     //console.log(rawDev.value);
