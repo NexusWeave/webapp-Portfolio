7a040b29789714f6bc0c6138bdfdebfa3204af42
krigjo25 <krigjo25@gmail.com>
2026-03-16 17:47:29 +0100

    feat(UI): 💄 ADD ID autoincrement & tag handling to `blogPost.ts`.     -   Implement autoincrementing ID for blog posts to ensure unique     identifiers.     -   Add functionality to handle tags associated with blog posts,     allowing better categorization and filtering of content.

--- frontend/composables/maps/blogPost.ts
+++ frontend/composables/maps/blogPost.ts
@@ -1,32 +1,37 @@
-
-import type { DevPostsCollectionItem } from '@nuxt/content';
 import type { DateItem } from '~/types/props';
+import type { DevPostsCollectionItem } from '@nuxt/content';
 
 export const mapBlogData = (data: DevPostsCollectionItem[]) => {
         if (!data) return [];
-        data = data.sort((a, b) => {
-            const dateA = new Date(a.date);
-            const dateB = new Date(b.date);
-            return dateB.getTime() - dateA.getTime();
-        });
-        
+        data = data.sort((a, b) => { const dateA = new Date(a.date); const dateB = new Date(b.date); return dateB.getTime() - dateA.getTime(); });
+
+        let AUTOINCREMENT = 0;
         return data.map((item) => {
+            const id = item.id.split('/')
             const date:DateItem = {date: item.date};
-            const id = item.id.split('/').pop()?.replace('.md', '').toLocaleLowerCase();
+            const path = id.pop()?.replace('.md', '').toLowerCase();
+            const tags = (() =>
+                    {
+                        const list_of_tags = ['project', 'devops', 'os', 'rd', 'mentalt-vedlikehold'];
+                        const index = id.findIndex(p => list_of_tags.includes(p.toLocaleLowerCase()));
+
+                        if (index === -1) return ['misc'];
+                        if (!id[index]) return;
+                        else if (id[index].toLowerCase() === 'project') { return [id[index + 1]];}
+                        else  return index != -1 ? [id[index]]: [];
+                    })()
             return {
-                id: id,
-                date: setDateFormat(date),
+                path: path,
+                tags: [tags],
+                title: item.title,
+                id: AUTOINCREMENT++,
                 star: item.star ?? '',
                 body: item.body ?? '',
-                title: item.title,
+                date: setDateFormat(date),
                 parade: item.parade ?? '',
                 ingress: item.ingress ?? '',
                 sources: item.sources ?? '',
-                anchor: 
-                [{
-                    href: `aktuelt/artikkel/${id}`, 
-                    label: 'Les mer'
-                }]
+                anchor: [{ href: `aktuelt/artikkel/${path}`, label: 'Les mer' }]
             }
         });
     }
