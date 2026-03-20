b161aa7245a154e322188a389aae4d1963949b9d
krigjo25 <krigjo25@gmail.com>
2026-03-20 14:46:25 +0100

    refactor(UI): ♻️ Improved the link system with safty & decoupling isMedia     -   Modernized the link system by implementing type-hints, decoupled     isMedia into standalone functions for images, icons, downloadable files, and     documents, enhancing code clarity and maintainability.
    Key Changes:
        -   Modernized the link system by integrating type-hints, that catches
        errors automatically, this reduces error margins, and enhances code clarity.
    
        -   Seperated isMedia, into standalone functions for images, `isIcon`,
        `isDownloadable` and `isDocument`, improving the overall readability
        and maintainability.
    
        -   ADD computed properties for props definitions.
        -   Resolved some unexpected behavior with external links, by defining
        external links as `http`, `https`, `tel:` or `mailto` and add a check
        if the condition is met, then the link will be treated as external,
        this ensures that the link system is reliable and can handle a
        wider range of link types effectively.

--- frontend/components/navigation/Anchor.vue
+++ frontend/components/navigation/Anchor.vue
@@ -3,108 +3,97 @@
         :class="cls[0]"
         :href="data.href"
         :aria-disabled="!!isDisabled()"
+        :download="isDownload() ? '' : null"
         :data-external-link="!!isExternal()"
         :target="isExternal() ? '_blank' : '_self'"
-        :rel="isExternal() ? 'noopener noreferrer' : null"
-        :download="isMedia() === 'download' ? data.label + '.' + data.type[0] : null"
+        :rel="isExternal() ? 'noopener noreferrer' : undefined"
         >
 
-        <template v-if="isMedia() === 'img'">
-            <MediaFigure :data="img" :cls="img.cls" />
-        </template>
+        <span v-if="isImage()">
+            <MediaFigure v-if="img" :data="img" :cls="img.cls" />
+        </span>
 
-        <template v-else-if="isMedia() == cls[cls.length - 1]">
-            <MediaIcon :type="data.type[0]" :label="data.label" :cls="[cls[cls.length - 1], data.type[0]]"/>
+        <span v-else-if="isIcon()" class="icon">
+            <MediaIcon :label="data.label" :cls="data.type[0]"/>
             {{ data.label }}
-        </template>
+        </span>
 
-        <template v-else>
-                {{ data.label }}
-        </template>
+        <span v-else> {{ data.label }} </span>
     </a>
 
 </template>
 
-<script setup>
+<script lang="ts" setup>
 
-    const props = defineProps({
-        data: {
-            type: Object,
-            required: true
-        },
-        cls: {
-            type: [Array],
-            required: false,
-        },
-        img: {
-            type: Object,
-            required: false,
-        }
-    });
+    //  --- Importing dependencies
+    import { computed } from 'vue';
 
-    const media = 
+    //  --- Importing Types
+    import type { FigureItem } from '~/types/props';
+
+
+    interface AnchorProps
     {
-        files: ['pdf'],
-        images: ['jpg', 'jpeg', 'png', 'svg', "webp"],
-        downloadFiles: ['docx', 'xlsx', 'csv'],
-        icons: ['mail', 'telephone', 'school', 'globe', 'map-pin', 'diploma', 'github', 'ytube', 'linkedin', 'facebook', 'instagram'],
+        data: {
+            href: string;
+            label: string;
+            type?: string[];
+            img?: FigureItem;
+            isDisabled?: boolean;
+        };
+        cls?: string[];
     }
 
-    const data = props.data;
-    const img = !!data.img ? data.img : null;
+    //  --- Defining Props
+    const props = withDefaults(defineProps<AnchorProps>(), {
+        cls: () => [],
+        type: () => [],
+        img: () => null, isDisabled: () => false,
+    });
 
-    const classList = () => {
-        const cls = (Array.isArray(data.cls) ? data.cls : [data.cls]);
-        cls.push('icon');
-
-        return cls;
-    };
-    
-    const cls = classList();
+    const data = computed(() => props.data);
+    const img = computed(() => data.value.img ?? null);
+    const cls = computed(() => Array.isArray(props.cls) ? props.cls : [props.cls]);
     
     const isExternal = () => {
-        if (!data.type) return false;
+        const dataProps = data.value;
+        if (!dataProps.href) return false;
 
-        const external = 'external';
-        return !!Array.isArray(data.type) ? data.type.includes(external) : data.type === external;
+        const external_links = ['https://', 'http://', 'mailto:', 'tel:'];
+        return external_links.some(link => dataProps.href.includes(link));
     };
 
     const isDisabled = () => {
-        if (!data.type) return false;
-        return !!Array.isArray(data.type) ? data.type.includes('disabled') : data.type === 'disabled';
+        const dataProps = data.value;
+        if (!dataProps.isDisabled) return false;
+
+        return !!dataProps.isDisabled 
     };
 
-    const isMedia = () => {
+    const isDownload = () => {
+        const dataProps = data.value;
+        if (!Array.isArray(dataProps.type)) return;
 
-        if (!data.type && !img) return false;
-
-        const search = Array.isArray(data.type) ? data.type[0] : data.type ? data.type : img.type ?? null
-
-        if (!search) return false;
-        else if (search === 'external') return false;
-
-        const files = media.files.find(item => item === search);
-        const icons = media.icons.find(item => item === search);
-        const images = media.images.find(item => item === search);
-        const download = media.downloadFiles.find(item => item === search);
-        //console.log("Search type:", search, "Data type:", data.type, "Image type:", img ? img.type : 'No image');
-        
-        switch (search)
-        {
-            case files || icons:
-                return cls[cls.length - 1];
-
-            case download:
-                return 'download';
-
-            case images:
-                return 'img';
-
-            default:
-                //console.warn('Data type is Not a media type:', search, data);
-                return false;
-        }
+        const downloadAble: string[] = ['docx', 'xlsx', 'csv' ];
+        return downloadAble.some(type => dataProps.type && dataProps.type.includes(type));
     };
+
+    const isImage = () => {
+        const dataProps = data.value;
+        if (!dataProps.type) return false;
+
+        const imageTypes = ['jpg', 'jpeg', 'png', 'svg', "webp"];
+        return imageTypes.some(type => dataProps.type && dataProps.type.includes(type));
+    };
+
+    const isIcon = () => {
+        const dataProps = data.value;
+        if (!dataProps.type) return false;
+
+        const iconTypes = ['mail', 'telephone', 'school', 'globe', 'map-pin', 'diploma', 'github', 'ytube', 'linkedin', 'facebook', 'instagram'];
+        return iconTypes.some(type => dataProps.type && dataProps.type.includes(type));
+    };
+
     //  --- Debug logic
     //console.log("Link component loaded with data: ", data, img);
 </script>
