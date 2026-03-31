ce787cee7f4b4c1a5989e535779e26c8e7da88cf
krigjo25 <krigjo25@gmail.com>
2026-03-31 13:15:00 +0200

    feat(HealthCheck): ✨ ADD health Check for Scanner class.

--- backend/lib/settings/api_config.py
+++ backend/lib/settings/api_config.py
@@ -59,10 +59,6 @@ class AsyncAPIClientConfig(WebAPIModel):
                 LOG.warn(f"Request was not successful.\n {e.__class__.__name__} Error Message: {e}. Time elapsed: {perf_counter()-start}\n")
                 raise e
 
-    @staticmethod
-    def timeout_config (standard: float = 120.0) -> httpx.Timeout:
-        return httpx.Timeout(standard)
-
     async def calculate_n(self, endpoint: str, header: Dict[str, str]): return await self.ApiCall(endpoint = f"{endpoint}", head = header)
 
     async def wait_in_queue(self, coro: Coroutine[Any, Any, T]) -> T:
@@ -73,35 +69,51 @@ class AsyncAPIClientConfig(WebAPIModel):
             LOG.error(f"Error in wait_in_queue: {e.__class__.__name__} - {str(e)}")
             raise e
 
+    @staticmethod
+    def timeout_config (standard: float = 120.0) -> httpx.Timeout:
+        return httpx.Timeout(standard)
+
 class Scanner(AsyncAPIClientConfig):
     def __init__(self, URL: str, KEY: Optional[str], version: str | None = None):
 
         self.URL = URL
-        if not KEY:
-            self.KEY = ''
-
-        super().__init__(URL=self.URL, KEY=self.KEY)
-        
-
-    async def fetch_web_rules(self):
-        site_map: str = f"{self.URL}/sitemap.xml"
-        header = {
+        self.HEADER = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             "Accept": "application/xml, text/xml, */*"
         }
-        site_urls: List[str] = []
+        if not KEY: self.KEY = ''
 
+        super().__init__(URL=self.URL, KEY=self.KEY)
+
+    async def check_status(self, path: str) -> bool:
         try :
-            res: httpx.Response = await self.ApiCall(endpoint = site_map, head = header)
+            res: httpx.Response = await self.ApiCall(endpoint = path, head = self.HEADER)
 
         except Exception as e: 
-            LOG.critical(f'Crawling not successfull - {e}')
+            LOG.critical(f'Crawling not successfull - {e.__class__.__name__} - {str(e)}')
+            return False
+        return res.status_code == 200
+
+
+    async def fetch_web_rules(self, site_map: str = '/sitemap.xml') -> Optional[List[str]]:
+
+        try :
+            path: str = urljoin(self.URL, site_map)
+            res: httpx.Response = await self.ApiCall(endpoint = path, head = self.HEADER)
+
+        except Exception as e: 
+            LOG.critical(f'Crawling not successfull - {e.__class__.__name__} - {str(e)}')
             return
 
+        site_urls: List[str] = []
         parse_xml = BeautifulSoup(res.text, 'xml')
+        disallowed_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp', '.mp4', '.avi', '.mov', '.wmv', '.flv', '.mp3', '.wav', '.ogg', '.pdf', '.docx', '.xlsx', '.pptx']
 
         for loc in parse_xml.find_all('loc'):
-            site_urls.append(loc.get_text())
+            if loc.get_text() not in site_urls and not any(loc.get_text().endswith(ext) for ext in disallowed_extensions):
+                site_urls.append(loc.get_text())
 
-        LOG.debug(site_map)
         return site_urls
+
+    async def fetch_web_information(self, endpoint: str): pass
+    async def strip_web_elements(self, html_content: str): pass
