9a2b7989dbf4f4f6c78ac0e30c2bf56a4683ee1e
krigjo25 <krigjo25@gmail.com>
2026-04-02 14:08:02 +0200

    refactor(API): ♻️ Enhanced scanning capabilities and improved error handling.     -   Integrated concurrent data fetching, which significantly     increases processing speed and system throughput.
        -   Implemented duplicate endpoint filtering, which ensures data
        integrity and prevents redundant processing cycles.
    
        -   Isolated connection handling within API calls, which guarantees
        fresh requests and improves overall connection reliability.
    
        -   Upgraded error reporting with status codes and request details,
        which reduces troubleshooting time and improves system visibility.
    
        -   Expanded scanning functionality with automated crawling and
        extraction, which enables the platform to gather more
        comprehensive insights.
    
        -   transformed web element processing into a static utility, which
        simplifies the codebase and lowers future maintenance.

--- backend/lib/settings/api_config.py
+++ backend/lib/settings/api_config.py
@@ -1,6 +1,6 @@
 
 #   Standard Libraries
-from asyncio import Semaphore
+from asyncio import Semaphore, gather
 from time import perf_counter
 from urllib.parse import urljoin
 from typing import  Coroutine, Optional, Dict, Any, TypeVar, List
@@ -24,13 +24,14 @@ T = TypeVar("T")
 
 class AsyncAPIClientConfig(WebAPIModel):
 
+    __VERSION__ = "v1.1.0"
+
     def __init__(self, URL:str, KEY: str, version: Optional[str] = None):
         self.API_URL = URL
         self.API_KEY = KEY
         self.VERSION = version
         self.QUEUE: int = 5
         self.SEM = Semaphore(self.QUEUE)
-        self.client = httpx.AsyncClient(timeout=self.timeout_config())
 
     async def ApiCall(self, endpoint: Optional[str], head: Dict[str, str], params: Optional[Dict[str, str | int]] = None) ->  httpx.Response:
 
@@ -40,23 +41,24 @@ class AsyncAPIClientConfig(WebAPIModel):
         start = perf_counter()
         path:str = self.API_URL
 
-        if endpoint:
-            path = urljoin(self.API_URL,endpoint)
+        protocol = ['https://', 'http://']
+        if endpoint and any(endpoint.startswith(i) for i in protocol): path = endpoint
+        else :path = urljoin(self.API_URL,endpoint)
 
-        async with self.client as cli:
+        async with httpx.AsyncClient(timeout=self.timeout_config(), follow_redirects = True) as cli:
             try:
 
                 req: httpx.Response = await cli.get(url = path, headers=head, params=params)
 
                 match req.status_code:
                     case 200: return req
-                    case 404: raise HTTPError('Resource not found')
-                    case 408 | 504: raise TimeOutError(req.status_code, None)
-                    case 401 | 403: raise ConnectionError('Unauthorized Access')
-                    case _: raise RequestError(f"Unexpected status code: {req.status_code}")
+                    case 404: raise HTTPError(f"{req.status_code} - {req.text}")
+                    case 408 | 504: raise TimeOutError(req.status_code, req.text)
+                    case 401 | 403: raise ConnectionError(f"{req.status_code} - {req.text}")
+                    case _: raise RequestError(f"Unexpected status code: {req.status_code} - {req.text}")
 
             except (HTTPError, ConnectionError, TimeOutError, RequestError) as e: 
-                LOG.warn(f"Request was not successful.\n {e.__class__.__name__} Error Message: {e}. Time elapsed: {perf_counter()-start}\n")
+                LOG.warn(f"Request was not successful.\n {e.__class__.__name__} Error Message: {e}.\nTime elapsed: {perf_counter()-start} Endpoint used : {endpoint}\n Heading: {head}\n")
                 raise e
 
     async def calculate_n(self, endpoint: str, header: Dict[str, str]): return await self.ApiCall(endpoint = f"{endpoint}", head = header)
@@ -74,28 +76,30 @@ class AsyncAPIClientConfig(WebAPIModel):
         return httpx.Timeout(standard)
 
 class Scanner(AsyncAPIClientConfig):
+    __VERSION__ = "v1.0.0"
+
     def __init__(self, URL: str, KEY: Optional[str], version: str | None = None):
 
         self.URL = URL
         self.HEADER = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept": "application/xml, text/xml, */*"
         }
         if not KEY: self.KEY = ''
 
         super().__init__(URL=self.URL, KEY=self.KEY)
 
-    async def check_status(self, path: str) -> bool:
+    async def check_status(self) -> bool:
         try :
-            res: httpx.Response = await self.ApiCall(endpoint = path, head = self.HEADER)
+            data = await self.crawl_extract_information()
+            if not data: raise ValueError(f'Data not found')
 
         except Exception as e: 
             LOG.critical(f'Crawling not successfull - {e.__class__.__name__} - {str(e)}')
-            return False
-        return res.status_code == 200
+            raise
 
+        return True
 
-    async def fetch_web_rules(self, site_map: str = '/sitemap.xml') -> Optional[List[str]]:
+    async def fetch_web_rules(self, site_map: str = '/sitemap.xml') -> List[str]:
 
         try :
             path: str = urljoin(self.URL, site_map)
@@ -103,17 +107,55 @@ class Scanner(AsyncAPIClientConfig):
 
         except Exception as e: 
             LOG.critical(f'Crawling not successfull - {e.__class__.__name__} - {str(e)}')
-            return
+            raise e
 
         site_urls: List[str] = []
         parse_xml = BeautifulSoup(res.text, 'xml')
         disallowed_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp', '.mp4', '.avi', '.mov', '.wmv', '.flv', '.mp3', '.wav', '.ogg', '.pdf', '.docx', '.xlsx', '.pptx']
 
         for loc in parse_xml.find_all('loc'):
-            if loc.get_text() not in site_urls and not any(loc.get_text().endswith(ext) for ext in disallowed_extensions):
-                site_urls.append(loc.get_text())
+            url = loc.get_text()
+            if url not in site_urls and not any(url.endswith(ext) for ext in disallowed_extensions):
+                site_urls.append(url)
+
 
         return site_urls
 
-    async def fetch_web_information(self, endpoint: str): pass
-    async def strip_web_elements(self, html_content: str): pass
+    async def crawl_extract_information(self):
+
+        urls = await self.fetch_web_rules()
+        
+        tasks = [self.wait_in_queue(self.scrape_information(url)) for url in urls ]
+        results = await gather(*tasks)
+
+        #LOG.info(f"{urls}")
+        #LOG.info(f"{[r for r in results if r['status'] == 200]}")
+        
+        return [r for r in results if r['status'] == 200]
+
+    async def scrape_information(self, url:str) -> Dict[str, str | int]:
+        response:httpx.Response
+        dictionary: Dict[str,str | int] = {"url": url}
+        try:
+            response = await self.ApiCall(endpoint=url, head = self.HEADER)
+
+            dictionary['content'] = await self.strip_web_elements(response.text)
+            dictionary['status'] = response.status_code
+            LOG.info(f"{dictionary}")
+            return dictionary
+
+        except Exception as e:
+
+            dictionary['status'] = 'Status not fetched'
+            LOG.error(f"Error with scraping occured at host {url} - {e}")
+            return dictionary
+
+    @staticmethod
+    async def strip_web_elements(content: str) -> str:
+        desposible_elements: List[str] = ['script', 'style', "nav", "footer", "header"]
+        soup = BeautifulSoup(content, 'lxml')
+
+        for element in soup(desposible_elements):
+            element.decompose()
+
+        return " ".join(soup.stripped_strings)
