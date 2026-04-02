333004bbe656cd956f2b5cd574fe98bf3da49fb6
krigjo25 <krigjo25@gmail.com>
2026-04-02 14:09:58 +0200

    chore(HealthCheck): ✨ ADD module for health_check.

---
+++ backend/lib/utils/health_check.py
@@ -0,0 +1,43 @@
+#   Standard Libraries
+import __future__
+from typing import Dict
+
+#   Third-Party Libraries
+
+#   Internal Libraries
+from lib.settings.api_config import Scanner
+from lib.utils.logger_config import AppWatcher
+from lib.services.github.github_api import GithubAPI
+# Initialize the logger
+LOG = AppWatcher(dir="logs", name='Health-Check')
+LOG.file_handler()
+
+async def check_github_api() -> Dict[str, str]:
+    """ Checks the availability of the GitHub API. """
+    
+
+    github = GithubAPI(URL = 'https://krigjo25.no', KEY = '')
+
+    dictionary:Dict[str, str] = { "Name": github.__class__.__name__, "API version": github.__VERSION__ }
+    dictionary['status'] = "NOT OK"
+    return dictionary
+
+
+async def check_specializt_api() -> Dict[str, str]:
+    from lib.settings.api_config import Scanner
+
+    cb = Scanner(URL = 'https://krigjo25.no', KEY = '')
+
+    dictionary:Dict[str, str] = { "Name": cb.__class__.__name__, "API version": cb.__VERSION__ }
+    try:
+
+        response:bool = await cb.check_status()
+        if not response: raise Exception('Response is none')
+
+    except Exception as e:
+        LOG.error(f"Specialist API check failed: {e.__class__.__name__} - {str(e)}")
+        dictionary['status'] = "NOT OK"
+        return dictionary
+
+    dictionary['status'] = "OK"
+    return dictionary
