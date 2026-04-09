6a8655d7829642efde0946cb053e5f47fb4f5e65
krigjo25 <krigjo25@gmail.com>
2026-04-09 16:09:09 +0200

    refactor(UI): ♻️ Simplified profile-bar & optimized navigation logic.     -   Simplified the logo menu logic, which reduces code complexity     and ensures a responsive, reliable interaction for users navigating     the site.
        -   Updated the profile handling logic, which improves data
        synchronization and provides a more consistent display of
        user-specific information.
    
        -   Pruned unused UI sections, which declutters the component and
        improves page performance by removing unnecessary DOM elements.
    
        -   Enhanced component maintainability, which makes the `profile-bar`
        easier to update and customize as the platform's navigation
        requirements evolve.

--- frontend/components/layout/Header.vue
+++ frontend/components/layout/Header.vue
@@ -4,37 +4,20 @@
             <NavigationNavMenu :data="data.logo" :cls="['logo-bar', 'logo-list', 'logo-item']" toggle="anchor" />
         </section>
         <section class="flex-wrap-row-align-items-center-justify-center">
-            <NavigationNavMenu :data="data.menu" toggle="router"/>
+            <NavigationNavMenu :data="menu" toggle="router"/>
             <NavigationAnchor :data="btn"/>
         </section>
-        <section class="flex-wrap-row-align-items-center profile">
-            <section class="profile-bar">
-                <section v-for="(data, i) in sortedReference" :key="i">
-                        <article v-if="data.isVisible">
-                            <MediaFigure
-                                :data="{ type: 'jpg', src: 'media/images/carousel/20240903_165612.jpg', alt: 'Portrett av Kristoffer Gjøsund', caption: data.quote}"
-                                :cls="['profile-avatar']"
-                            />
-                            <h4>-
-                                <cite>
-                                    <NavigationAnchor 
-                                        :data="data.anchor"
-                                    />
-                                </cite>
-                            </h4>
-                        </article>
-                    </section>
-                
+        <article class="profile-bar flex-wrap-row-align-items-center profile">
+            <section class="flex-column-align-items-center">
+                <MediaFigure :data="media" />
+                <q>{{ sortedReference[activeIndex]?.quote }}</q>
+                <cite> <NavigationAnchor :data="sortedReference[activeIndex]?.anchor" /> </cite>
             </section>
-            <article class="flex-column">
+            <section class="profile-content flex-column">
                 <h2>Kristoffer Gjøsund</h2>
                 <section class="slogan-wrapper">
-                    <h2 class="flex-wrap-row-justify-center slogan">
-                        <span v-for="text in data.title" :key="text">{{ text }} </span>
-                    </h2>
-                    <p>
-                        Forener min akademiske reise fra bygg, helse og IT. Til å skape løsninger gjennom samarbeid. For meg er utfordringer en felles reise
-                    </p>
+                    <h3 class="flex-wrap-row-justify-center slogan"> <span v-for="text in data.title" :key="text">{{ text }} </span> </h3>
+                    <p> Forener min akademiske reise fra bygg, helse og IT. Til å skape løsninger gjennom samarbeid. For meg er utfordringer en felles reise </p>
                 </section>
                 <section class="flex-column-align-items-center some">
                     <h3>Sosial media Linker / Kontakt</h3>
@@ -43,53 +26,47 @@
                         :cls="[['some-bar', 'flex-wrap-row'],['some-ul', 'flex-wrap-row'],'some-li']"
                     />
                 </section>
-            </article>
-        </section>
+            </section>
+        </article>
     </section>
     
 </template>
 <script lang="ts" setup>
 
     //  --- Import & types logic
-    import { startTimer } from '~/utils/utils';
-    import { onMounted, onUnmounted, fetchCollection } from '#imports';
+    
+    import { onMounted, fetchCollection } from '#imports';
 
     import type { ReferenceCollectionItem } from '@nuxt/content';
-    const data =
+    import { mapReference } from '~/composables/maps/mapReferences';
+
+    const data = computed(() =>
     {
-        title: [ 'Logic', 'Meets', 'Creative', 'Solutions'],
-        logo: 
-        {
-            anchor:
-            { href: '/', cls: ['nav-link-logo'],
-                img:
-                {
-                    type: 'png',
-                    cls: ['k-logo-img'],
-                    alt: 'logic-meets-creative-solutions.png',
-                    src: '/media/images/logo/logic-meets-creative-solutions.png',
-                }
-            },
-            
-            
-        },
-        menu: [ { href: '/', label: 'Portfolio' }, { href: '/dev', label: 'Dev Profile' }, { href: '/personal', label: 'Om Kristoffer' } ]
-    }
+        const title = [ 'Logic', 'Meets', 'Creative', 'Solutions']
+        const image = '/media/images/logo/logic-meets-creative-solutions.png'
+        const logo =  { anchor: { href: '/', media: { caption:' ', src: image, type: 'png', cls: ['k-logo-img'], alt: `${image.split('/').pop()?.split('.').shift()} logo` } } };
+        
+        return { title: title, logo: logo};
+    });
+    const menu = [{ href: '/', label: 'Portfolio' }, { href: '/dev', label: 'Dev Profile' }, { href: '/personal', label: 'Om Kristoffer' }];
+    
     const btn = { id: 0, label: 'CV & Portefølje ', cls: ['btn', 'pdf-btn'], href: '/media/documents/CV-Portofolio.pdf', type: ['pdf'] };
-
     const SocialMedia = [ { type: ['linkedin'], href: 'https://www.linkedin.com/in/krigjo25' }, { type: ['github'],  href: 'https://www.github.com/krigjo25' }, { type: ['mail','external'], href: 'mailto:krigjo25@outlook.com' }, { type: ['ytube','external'], href: 'https://www.youtube.com/@krigjo25' }, { type: ['facebook'], href: 'https://www.facebook.com/krigjo25' }, { type: ['instagram'], href: 'https://www.instagram.com/krigjo25' } ];
-
-
-    //useHead({ script: [ { src: 'https://www.googletagmanager.com/gtag/js?id=G-4XX727FZCG', async: true }, { innerHTML: ` window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-4XX727FZCG'); `, type: 'text/javascript' } ] });
 
     const referencePath = 'reference';
     const referenceCache = 'referenceCache';
     const reference = await fetchCollection<ReferenceCollectionItem>(referencePath, referenceCache);
-    const sortedReference = reactive(mapReference(reference));
+    const media = { type: 'jpg', src: 'media/images/carousel/20240903_165612.jpg', srcset: 'media/images/carousel/20240903_165612.jpg', alt: 'Portrett av Kristoffer Gjøsund'}
 
-    let timerInterval: ReturnType<typeof setInterval> | null = null;
+    const activeIndex = ref(0);
+    const sortedReference = computed(() => {
+        const data = mapReference(reference);
+        return data.sort((a, b) => a.id - b.id);
+    });
+    const { index, start } = useCarousel(sortedReference.value.length, 10000);
 
     // --- Lifecycle Logic
-    onMounted(() => { timerInterval = startTimer(sortedReference); });
-    onUnmounted(() => { if (timerInterval) { clearInterval(timerInterval); }});
+    watch(index, (newIndex) => { activeIndex.value = newIndex; //console.log('Current index:', newIndex);
+    });
+    onMounted(() => { start(); });
 </script>
