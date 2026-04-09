dd7ca6b91a9db2bb3eb83fe537f322c8f8ec4736
krigjo25 <krigjo25@gmail.com>
2026-04-09 15:56:31 +0200

    refactor(Utils): ♻️ Modernized timer logic & moved -> composables.     -   Moved `startTimer` as a new composable utility, which improves     code organization by moving logic closer to where it is reactive and     functional. Renamed `startTimer` -> `useCarousel`**, which more     accurately reflects its purpose and aligns with standard Vue naming     conventions for reusable logic and enhanced hook flexibility, which     allows the carousel logic to be reused across different components     with various timing and transition requirements.
        -   Removed legacy utilities which involved removing the redundant
        `next` function to reduce technical garbage and clean the codebase.
    
        -   Optimized state management, which provides a more robust and
        testable foundation for time-based UI transitions.
    
        -   Extracted `mapTimeline` and `mapReferences` functions,
        by separates data transformation logic from the core preprocessor to
        improve modularity and testability.
    
        -   simplified the preprocessing pipeline, which enhances code
        readability by allowing the main preprocessor to focus on orchestration
        rather than low-level mapping.
    
        -   Improved architectural separation, which provides a cleaner
        foundation for future data integrations and reduces the risk of side
        effects when updating mapping rules.

---
+++ frontend/composables/maps/mapTimeline.ts
@@ -0,0 +1,38 @@
+import type { FigureItem } from '~/types/props';
+import type { TimelineItem } from '~/types/timeline';
+import type { AcademicCollectionItem } from '@nuxt/content';
+
+export function mapTimeline(data: Ref<AcademicCollectionItem[]>): TimelineItem[] {
+    if (!data.value) return [];
+
+    let AUTOINCREMENT: number = 0;
+    const timeline = sortbyDate<AcademicCollectionItem>(data.value);
+
+    return timeline.map((doc: AcademicCollectionItem): TimelineItem => {
+        const techStack = fetchTechType(doc.techStack);
+        let tech: FigureItem[] | undefined = undefined;
+
+        if (techStack && techStack.length > 0) {
+            tech = techStack.map((item) => {
+                const label = item.label.toLowerCase();
+                const category = item.category.toLowerCase();
+
+                return { type: 'svg', frameWork: label, category: category, alt: 'A visual representation of ' + label, src: `/media/tech-lang-icons/${label}.svg`, srcset: `/media/tech-lang-icons/${label}.svg` } as FigureItem;
+            });
+        }
+
+        return {
+            techStack: tech as FigureItem[],
+            id: AUTOINCREMENT++,
+            body: doc.body || undefined, 
+            name: doc.tag + "-Timeline",
+            title: doc.title || undefined, 
+            isVisible: (AUTOINCREMENT - 1) === 0,
+            date: { created: doc.created, end: doc.end },
+            description: doc.meta.description || undefined,
+            location: { name: doc.location, anchor: { label: doc.location, href: doc.loc_link || undefined } },
+            organization: { name: doc.organization, anchor: { label: doc.organization, href: doc.org_link } },
+            reference: { name: doc.references, anchor: { label: doc.references, href: doc.ref_link || undefined } },
+        } as unknown as TimelineItem;
+    });
+}
