0e90581a0dc1914e456c2dd5b0d6ae6bdec07a19
krigjo25 <krigjo25@gmail.com>
2026-04-10 11:27:17 +0200

    refactor(UI): ♻️ Optimized header navigation & Component logic.     -	Renamed the `data` variable to `logo` in `Layout/Header.vue` and     implemented explicit types for `logo`, `anchorMenu`, `menu`, and     `SoscialMedia`.
        -	Separated anchor links from router links in the header to
        improve routing efficiency within `NavMenu`.
    
        -	Replaced `NavigationAnchor` with `NavigationNavMenu` for main
        menu anchors and removed unused classes in `Layout/Header.vue`.
    
        -	Added `aria-label` support to `Anchor.vue` and renamed generic
        image attributes to `media` Improves naming consistency.
    
        -	Relocated `ButtonItem` and `ButtonProps` to `Navigation.d.ts`
        and added computed logic for classes and action state flags in
        `Button.vue`.
    
        -	Simplified `NavMenu.vue` by removing redundant template wrappers
        and refactoring the item type detection logic.
    
        -	Refactored pagination in `Index.vue` by replacing `PageButtons`
        with explicit `lastpagebtn` and `nextPageBtn` objects.
    
        -	Updated import references for `mapTimeline` and `ButtonItem`
        while stripping redundant wrappers and debug logic in `Index.vue`.
    
        -	Encapsulated general logic across all modified files to improve
        modularity and code reuse.

--- frontend/components/layout/Header.vue
+++ frontend/components/layout/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="flex-wrap-row-align-items-center-justify-space-around">
         <section>
-            <NavigationNavMenu :data="data.logo" :cls="['logo-bar', 'logo-list', 'logo-item']" toggle="anchor" />
+            <NavigationNavMenu :data="[logo.anchor]" />
         </section>
         <section class="flex-wrap-row-align-items-center-justify-center">
-            <NavigationNavMenu :data="menu" toggle="router"/>
-            <NavigationAnchor :data="btn"/>
+            <NavigationNavMenu :data="menu"/>
+            <NavigationNavMenu :data="anchorMenu" :cls="['anchor-nav']"/>
         </section>
         <article class="profile-bar flex-wrap-row-align-items-center profile">
             <section class="flex-column-align-items-center">
@@ -16,15 +16,12 @@
             <section class="profile-content flex-column">
                 <h2>Kristoffer Gjøsund</h2>
                 <section class="slogan-wrapper">
-                    <h3 class="flex-wrap-row-justify-center slogan"> <span v-for="text in data.title" :key="text">{{ text }} </span> </h3>
+                    <h3 class="flex-wrap-row-justify-center slogan"> <span v-for="text in logo.title" :key="text">{{ text }} </span> </h3>
                     <p> Forener min akademiske reise fra bygg, helse og IT. Til å skape løsninger gjennom samarbeid. For meg er utfordringer en felles reise </p>
                 </section>
                 <section class="flex-column-align-items-center some">
                     <h3>Sosial media Linker / Kontakt</h3>
-                    <NavigationNavMenu :data="SocialMedia"
-                        toggle="anchor"
-                        :cls="[['some-bar', 'flex-wrap-row'],['some-ul', 'flex-wrap-row'],'some-li']"
-                    />
+                    <NavigationNavMenu :data="SocialMedia" :cls="['some-nav']"/>
                 </section>
             </section>
         </article>
@@ -34,24 +31,23 @@
 <script lang="ts" setup>
 
     //  --- Import & types logic
-    
     import { onMounted, fetchCollection } from '#imports';
 
     import type { ReferenceCollectionItem } from '@nuxt/content';
     import { mapReference } from '~/composables/maps/mapReferences';
+    import type { AnchorItem, RouterItem } from '~/types/navigation';
 
-    const data = computed(() =>
+    const logo = computed(() =>
     {
-        const title = [ 'Logic', 'Meets', 'Creative', 'Solutions']
-        const image = '/media/images/logo/logic-meets-creative-solutions.png'
-        const logo =  { anchor: { href: '/', media: { caption:' ', src: image, type: 'png', cls: ['k-logo-img'], alt: `${image.split('/').pop()?.split('.').shift()} logo` } } };
+        const title: string[] = [ 'Logic', 'Meets', 'Creative', 'Solutions']
+        const image:string = '/media/images/logo/logic-meets-creative-solutions.png'
+        const anchor: AnchorItem =  { href: '/', media: { caption:' ', src: image, srcset: image, type: 'png', alt: `${image.split('/').pop()?.split('.').shift()} logo` } };
         
-        return { title: title, logo: logo};
+        return { title: title, anchor: anchor};
     });
-    const menu = [{ href: '/', label: 'Portfolio' }, { href: '/dev', label: 'Dev Profile' }, { href: '/personal', label: 'Om Kristoffer' }];
-    
-    const btn = { id: 0, label: 'CV & Portefølje ', cls: ['btn', 'pdf-btn'], href: '/media/documents/CV-Portofolio.pdf', type: ['pdf'] };
-    const SocialMedia = [ { type: ['linkedin'], href: 'https://www.linkedin.com/in/krigjo25' }, { type: ['github'],  href: 'https://www.github.com/krigjo25' }, { type: ['mail','external'], href: 'mailto:krigjo25@outlook.com' }, { type: ['ytube','external'], href: 'https://www.youtube.com/@krigjo25' }, { type: ['facebook'], href: 'https://www.facebook.com/krigjo25' }, { type: ['instagram'], href: 'https://www.instagram.com/krigjo25' } ];
+    const anchorMenu:AnchorItem[] = [{ label: 'CV & Portefølje ', href: 'https://krigjo25.no/media/documents/CV-Portofolio.pdf', type: ['pdf'] }];
+    const menu: RouterItem[] = [{ order: 0, type: ['router'], path: '/', label: 'Portfolio' }, { order: 1, type: ['router'], path: '/dev', label: 'Dev Profile' }, { order: 2, type: ['router'], path: '/personal', label: 'Om Kristoffer' }];
+    const SocialMedia: AnchorItem[] = [ { type: ['linkedin'], href: 'https://www.linkedin.com/in/krigjo25' }, { type: ['github'],  href: 'https://www.github.com/krigjo25' }, { type: ['mail','external'], href: 'mailto:krigjo25@outlook.com' }, { type: ['ytube','external'], href: 'https://www.youtube.com/@krigjo25' }, { type: ['facebook'], href: 'https://www.facebook.com/krigjo25' }, { type: ['instagram'], href: 'https://www.instagram.com/krigjo25' } ];
 
     const referencePath = 'reference';
     const referenceCache = 'referenceCache';
@@ -59,14 +55,11 @@
     const media = { type: 'jpg', src: 'media/images/carousel/20240903_165612.jpg', srcset: 'media/images/carousel/20240903_165612.jpg', alt: 'Portrett av Kristoffer Gjøsund'}
 
     const activeIndex = ref(0);
-    const sortedReference = computed(() => {
-        const data = mapReference(reference);
-        return data.sort((a, b) => a.id - b.id);
-    });
+    const sortedReference = computed(() => { const data = mapReference(reference); return data.sort((a, b) => a.id - b.id); });
+    //  --- Timer Logic
     const { index, start } = useCarousel(sortedReference.value.length, 10000);
 
     // --- Lifecycle Logic
-    watch(index, (newIndex) => { activeIndex.value = newIndex; //console.log('Current index:', newIndex);
-    });
+    watch(index, (newIndex) => { activeIndex.value = newIndex; });
     onMounted(() => { start(); });
 </script>
--- frontend/components/navigation/Anchor.vue
+++ frontend/components/navigation/Anchor.vue
@@ -2,99 +2,64 @@
     <a
         :class="cls"
         :href="data.href"
+        :aria-label="data.label"
         :aria-disabled="!!isDisabled()"
         :download="isDownload() ? '' : null"
         :data-external-link="!!isExternal()"
         :target="isExternal() ? '_blank' : '_self'"
         :rel="isExternal() ? 'noopener noreferrer' : undefined"
-        >
-
-        <span v-if="isImage()">
-            <MediaFigure v-if="img" :data="img" :cls="img.cls" />
-        </span>
-
+    >
+        <span v-if="isImage()"> <MediaFigure :data="media" :cls="cls" /> </span>
         <span v-else-if="isIcon()" class="icon">
             {{ data.label }}
-            <MediaIcon :cls="data.type[0]"/>    
+            <MediaIcon :cls="data.type"/>
         </span>
-
         <span v-else> {{ data.label }} </span>
     </a>
-
 </template>
 
 <script lang="ts" setup>
 
-    //  --- Importing dependencies
+    //  --- Importing dependencies & type definitions
     import { computed } from 'vue';
 
-    //  --- Importing Types
-    import type { FigureItem } from '~/types/props';
-
-
-    interface AnchorProps
-    {
-        data: {
-            href: string;
-            label: string;
-            type?: string[];
-            img?: FigureItem;
-            isDisabled?: boolean;
-        };
-        cls?: string[];
-    }
+    import type { AnchorProps } from '~/types/navigation';
 
     //  --- Defining Props
-    const props = withDefaults(defineProps<AnchorProps>(), {
-        cls: () => [],
-        type: () => [],
-        img: () => null, isDisabled: () => false,
-    });
-
+    const props = withDefaults(defineProps<AnchorProps>(), { cls: () => [], type: () => [], img: () => null, isDisabled: () => false });
+    const cls = computed(() => props.cls);
     const data = computed(() => props.data);
-    const img = computed(() => data.value.img ?? null);
-    const cls = computed(() => Array.isArray(props.cls) ? props.cls : undefined);
-    
-    const isExternal = () => {
-        const dataProps = data.value;
-        if (!dataProps.href) return false;
+    const media = computed(() => data.value.media?? null);
 
-        const external_links = ['https://', 'http://', 'mailto:', 'tel:'];
-        return external_links.some(link => dataProps.href.includes(link));
-    };
+    /// --- Computed Flags
+    const isExternal = () => { const dataProps = data.value; if (!dataProps.href) return false; const external_links = ['https://', 'http://', 'mailto:', 'tel:']; return external_links.some(link => dataProps.href.includes(link)); };
 
-    const isDisabled = () => {
-        const dataProps = data.value;
-        if (!dataProps.isDisabled) return false;
-
-        return !!dataProps.isDisabled 
-    };
+    const isDisabled = () => { const dataProps = data.value; if (!dataProps.isDisabled) return false; return dataProps.isDisabled  };
 
     const isDownload = () => {
         const dataProps = data.value;
         if (!Array.isArray(dataProps.type)) return;
 
         const downloadAble: string[] = ['docx', 'xlsx', 'csv' ];
+
         return downloadAble.some(type => dataProps.type && dataProps.type.includes(type));
     };
 
     const isImage = () => {
-        const imgProps = img.value;
+        const imgProps = media.value;
         const dataProps = data.value;
+
         if (!dataProps.type && !imgProps) return false;
+
         const imageTypes = ['jpg', 'jpeg', 'png', 'svg', "webp"];
+
         if (imageTypes.some(type => dataProps.type && dataProps.type.includes(type))) return true;
         if (imgProps && imgProps.type) return imageTypes.some(type => imgProps.type && imgProps.type.includes(type));
+
         return false;
     };
 
-    const isIcon = () => {
-        const dataProps = data.value;
-        if (!dataProps.type) return false;
-
-        const iconTypes = ['docs', 'pdf', 'mail', 'telephone', 'school', 'globe', 'map-pin', 'diploma', 'github', 'ytube', 'linkedin', 'facebook', 'instagram'];
-        return iconTypes.some(type => dataProps.type && dataProps.type.includes(type));
-    };
+    const isIcon = () => { const dataProps = data.value; if (!dataProps.type) return false; const iconTypes = ['docs', 'pdf', 'mail', 'telephone', 'school', 'globe', 'map-pin', 'diploma', 'github', 'ytube', 'linkedin', 'facebook', 'instagram']; return iconTypes.some(type => dataProps.type && dataProps.type.includes(type)); };
 
     //  --- Debug logic
     //console.log("Anchor component loaded with data: ", img.value, isImage());
