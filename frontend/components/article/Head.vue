e4c4299d600afd4f90841d953026228a6c5d48c6
krigjo25 <krigjo25@gmail.com>
2026-04-10 14:34:08 +0200

    refactor(navigation): replaced `UtilsTags` with `NavigationLink`.

--- frontend/components/article/Head.vue
+++ frontend/components/article/Head.vue
@@ -6,7 +6,7 @@
             <h1> {{ article.title }}</h1>
             <p class="flex-wrap-row-align-items-center-justify-center article-metadata">
                 <span v-if="!!article.date" :class="cls[3]"> Publisert: <b><time :datetime="article.date.date">{{ article.date.date }}</time></b></span>
-                <UtilsTags v-for="(tag) in article.tags" :data="tag" />
+                <NavigationAnchor v-for="(tag) in article.tags" :data="tag" :class="tag.cls"/>
             </p>
             <MDC :value="article.ingress" class="ingress-content" />
             <NavigationNavMenu v-if="!isArticlePage && !!article.anchor" :data="article.anchor" :cls="['nav-bar', 'read-more']"
@@ -32,9 +32,9 @@
     const button = computed<ButtonItem>(() => {  if (isArticlePage.value) return  { label: 'Gå tilbake', action: () => { window.history.back(); }}; return {} });
 
     //  --- Debugging logic
+    //console.log("Article Header Component - Article :", article.value);
     //console.log("Article Header Component - Button :", button.value);
     //console.log("Article Header Component - isNewsPage :", isNewsPage.value);
     //console.log("Article Header Component - isArticlePage :", isArticlePage.value);
-    //console.log("Article Header Component - Article :", article);
     //console.log("Article Header Component - Article Anchor :", article.anchor);
 </script>
