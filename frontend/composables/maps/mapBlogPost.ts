9268319e0556a86d287f9e860f0389489f0bf8cc
krigjo25 <krigjo25@gmail.com>
2026-04-10 12:03:22 +0200

    refactor(Blog): ♻️ Renamed wrapper utility & standardized property naming.     -	Renamed `blogPost.ts` -> `mapBlogPost.ts` which improves semantic     naming and clarity.
        -	Reorganized variable declarations to enhance code readability
        and logical flow.
    
        -	Changed the `href` attribute to `path` and applied the
        `read-more-btn` class to all relevant elements.
    
        -	Enforced consistent syntax by adding missing semicolons across
        the file.
    
        -	Renamed the `list_of_tag` variable to `listOfTags` to follow
        camelCase naming conventions.
    
        -	Encapsulated specific logic fragments to improve modularity and
        reduce global scope pollution.

--- frontend/composables/maps/mapBlogPost.ts
+++ frontend/composables/maps/mapBlogPost.ts
@@ -1,23 +1,24 @@
-import type { DateItem } from '~/types/props';
+import type { DateItem } from '~/types/date';
 import type { DevPostsCollectionItem } from '@nuxt/content';
 
 export const mapBlogData = (data: DevPostsCollectionItem[]) => {
         if (!data) return [];
         data = data.sort((a, b) => { const dateA = new Date(a.date); const dateB = new Date(b.date); return dateB.getTime() - dateA.getTime(); });
-        const today = new Date()
+        
         let AUTOINCREMENT = 0;
+        const today = new Date();
+        
         return data.map((item) => {
-            const isPublished = new Date(item.date) <= today
             const id = item.id.split('/')
             const date:DateItem = {date: item.date};
+            const isPublished = new Date(item.date) <= today;
             const path = id.pop()?.replace('.md', '').toLowerCase();
             const tags = (() =>
                     {
-                        const list_of_tags = ['project', 'devops', 'os', 'rd', 'mentalt-vedlikehold'];
-                        const index = id.findIndex(p => list_of_tags.includes(p.toLocaleLowerCase()));
+                        const listOfAvailableTags = ['project', 'devops', 'os', 'rd', 'mentalt-vedlikehold'];
+                        const index = id.findIndex(p => listOfAvailableTags.includes(p.toLocaleLowerCase()));
 
-                        if (index === -1) return ['misc'];
-                        if (!id[index]) return;
+                        if (index === -1) return ['misc']; if (!id[index]) return;
                         else if (id[index].toLowerCase() === 'project') { return [id[index + 1]];}
                         else  return index != -1 ? [id[index]]: [];
                     })()
@@ -33,7 +34,7 @@ export const mapBlogData = (data: DevPostsCollectionItem[]) => {
                 parade: item.parade ?? '',
                 ingress: item.ingress ?? '',
                 sources: item.sources ?? '',
-                anchor: [{ href: `artikkel/${path}`, label: 'Les mer' }]
+                anchor: [{ type: ['router'], path: `artikkel/${path}`, label: 'Les mer', cls: ['read-more-btn'] }]
             }
         });
     }
