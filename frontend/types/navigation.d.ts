27f6af27641665fc10ee9cb26b7451b49b8a335e
krigjo25 <krigjo25@gmail.com>
2026-04-10 16:37:00 +0200

    refactor(Types): ♻️ Specify string literial for button function.

--- frontend/types/navigation.d.ts
+++ frontend/types/navigation.d.ts
@@ -4,7 +4,7 @@ interface BaseItem { id?: string; label?: string; cls?: string[]; type?: string[
 
 export interface RouterItem extends BaseItem { path: string; order?: number; }
 export interface AnchorItem extends BaseItem { href: string; media?: FigureItem; isDownload?: boolean; isDisabled?: boolean;}
-export interface ButtonItem extends BaseItem { disabled?: boolean; anchor?: AnchorItem; action?: () =>  number | void; type?: "submit" | "reset"; }
+export interface ButtonItem extends BaseItem { disabled?: boolean; anchor?: AnchorItem; action?: () =>  string |number | void; type?: "submit" | "reset"; }
 
 export interface ButtonProps { cls?: string[]; data: ButtonItem; }
 export interface AnchorProps { data:  AnchorItem; cls?: string[]; }
