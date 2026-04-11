6036177a98ac80720859d764b87bc7742ac68082
krigjo25 <krigjo25@gmail.com>
2026-04-11 08:41:01 +0200

    fix: resolved issue with slug name, to define page as a logs-record

--- frontend/components/article/Page.vue
+++ frontend/components/article/Page.vue
@@ -1,10 +1,7 @@
 <template>
     <article class = 'article-wrapper flex-column'>
        <header>
-           <ArticleHead
-                :article="article"
-                :isNewsPage="isPage"
-                :isArticlePage="isPage"/>
+           <ArticleHead :article="article" :isNewsPage="isPage" :isArticlePage="isPage"/>
        </header>
        <main v-if="isPage">
         <ArticleBody :data="article" />
@@ -25,10 +22,10 @@
     const article = computed(() => props.data);
 
     const route = useRoute();
-    const isPage = computed(() => {return route.name?.toString().startsWith('artikkel')});
+    const isPage = computed(() => {return route.name?.toString().startsWith('logs-records')});
 
     //  --- Debugging logic
-    //console.log("Articles Component - Article Data :", route.name);
-    //console.log("Articles Component - isPage :", isPage.value);
+    console.log("Articles Component - Article Data :", route.name);
+    console.log("Articles Component - isPage :", isPage.value);
     //console.log("Articles Component :", isPage.value, article, isNewsPage, isArticlePage), article.conclusion;
 </script>
