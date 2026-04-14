17de4e6ae7e2afcd52761cf0302cbd2fe7649e0b
krigjo25 <krigjo25@gmail.com>
2026-04-14 16:46:31 +0200

    fix: Prevented unexpected behavior, by adding export for LanguageData.

--- frontend/types/props.d.ts
+++ frontend/types/props.d.ts
@@ -13,19 +13,14 @@ interface RepositoryBase
     flags: Record<string, boolean>;
     languages?: GithubRepoLanguage[];
 }
-export interface GithubData extends RepositoryBase
-{
-    id: string;
-    label: string;
-    date: DateItem;
-    
-}
 
-interface LanguageData { label: string; bytes: number; }
+
 interface ProgressItem extends LanguageData { type: string; percentage: number; }
 
+export interface LanguageData { label: string; bytes: number; }
 export interface RepositoryData extends Array<RepositoryItem> {}
 export interface ProgressProps { data: ProgressItem; cls?: string[]; }
 export interface GithubRepoLanguage extends LanguageData { img: FigureItem[];}
+export interface GithubData extends RepositoryBase { id: string; label: string; date: DateItem; }
 export interface RepoProps { data: GithubRepo; cls?: Array<string | string[] | Array<string | string[]>>; }
 export interface RepositoryItem extends RepositoryBase { label: string; repo_id: string; created_at: string; }
