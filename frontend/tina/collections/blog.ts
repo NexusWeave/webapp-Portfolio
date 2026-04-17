5d12cf3db8b432b1c376da16e0653f00cb834dc6
krigjo25 <krigjo25@gmail.com>
2026-04-17 14:20:02 +0200

    refactor(cms): Automatically ADD date.

--- frontend/tina/collections/blog.ts
+++ frontend/tina/collections/blog.ts
@@ -11,9 +11,11 @@ export const blogCollection: Collection =
         { name: "title", isTitle: true, type: "string", label: "Title", required: true },
         { required: true, name: "ingress", label: "Ingress", type: "rich-text" },
         { required: false, name: "status", type: "rich-text", label: "Dagens Aktiviteter og Status" },
-        { isBody: true, name: "body", required: true, 
-            type: "rich-text", label: "Innholdet i Artikkelen", 
-            description: "Hovedinnhold i artikkelen, i star-parade formatet" },
+        { isBody: true, name: "body", required: true, type: "rich-text", label: "Innholdet i Artikkelen", description: "Hovedinnhold i artikkelen, i star-parade formatet" },
         { required: false, name: "sources", type: "rich-text", label: "Kilde Henvisning" },
     ],
+    ui: {
+        beforeSubmit: async({values}) =>
+        { return { ...values, date: values.date ?? new Date().toISOString(), }}
+    }
 };
