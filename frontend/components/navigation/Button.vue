04c62a0cdd8f210e2ce80fee91204cc5f77efb00
krigjo25 <krigjo25@gmail.com>
2026-04-19 15:16:24 +0200

    refactor(components): integrate `isIcon` logic and media icons into `Buttons.vue`

--- frontend/components/navigation/Button.vue
+++ frontend/components/navigation/Button.vue
@@ -2,6 +2,10 @@
     <button :class="cls" :type="btn.type ?? 'button'" @click="isActionable()" :disabled="isDisabled">
 
         <NavigationAnchor v-if="btn.anchor" :data="btn.anchor"/>
+        <span v-else-if="isIcon()" class="icon">
+            {{ data.label }}
+            <MediaIcon :cls="data.type"/>
+        </span>
 
         <template v-else> {{ btn.label }} </template>
 
@@ -18,9 +22,11 @@
     const props = withDefaults(defineProps<ButtonProps>(), { cls: () => [], action: () => {}});
     const cls = computed(() => props.cls);
     const btn = computed(() => props.data);
+
     const isDisabled = computed(() => !!btn.value.disabled);
     const isActionable = computed(() => btn.value.action ? btn.value.action : () => {});
-    
+    const isIcon = () => { const dataProps = btn.value; if (!dataProps.type) return false; const iconTypes = ['docs', 'pdf', 'mail', 'telephone', 'school', 'globe', 'map-pin', 'diploma', 'github', 'ytube', 'linkedin', 'facebook', 'instagram','dir']; return iconTypes.some(type => dataProps.type && dataProps.type.includes(type)); };
+
 
     //  --- Watcher
     watch(() => props.data, (newValue) => { Object.assign(btn.value, newValue); }, { immediate: true });
