f173c795c5eb502d9480544b4c583a0d4df7bfa1
Krigjo25 <krigjo25@gmail.com>
2026-06-23 09:43:30 +0200

    test(composable): add unit test for useRotateCollections, verify timing & index boundaries.

--- frontend/tests/nuxt/preprosessor-utils.test.ts
+++ frontend/tests/nuxt/preprosessor-utils.test.ts
@@ -1,7 +1,8 @@
-import { describe, it, expect } from 'vitest';
-import { sortbyDate, setDateFormat, useCarousel, useNavigation } from '#imports';
+import { dummyApp } from '../utils/utils';
+import { sortbyDate, setDateFormat, useNavigation } from '#imports';
+import { useRotateCollections } from '~/composables/preprosessor-utils';
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
 import { sortObject, expectedSort, setDateFormatData, setDateFormatData_1, setDateFormatExpected,setDateFormatExpected_1 } from '../data/preprosessor-data';
-
 
 describe('Preprocessor Utils', () => {
     describe('SortbyDate()', () => {
@@ -24,9 +25,46 @@ describe('Preprocessor Utils', () => {
         });
     });
 
-    describe('useCarosuel()', () => {
-        it('', () => {
-            expect(true).toBe(true);
+    describe('useRotateCollections()', () => {
+        const timer = 1000;
+        const index: number = 5;
+
+        beforeEach(() => vi.useFakeTimers() );
+        afterEach(() => vi.useRealTimers() );
+
+        it('Initialize random index within bounds', () => {
+            const [carousel] = dummyApp(() => useRotateCollections(index))
+
+            const value = carousel.index.value;
+            expect(value).toBeLessThanOrEqual(5);
+            expect(value).toBeGreaterThanOrEqual(0);
+
+        });
+
+        it('Increase index over time when started', () => {
+            const [carousel] = dummyApp(() => useRotateCollections(index, timer));
+            const value = carousel.index.value;
+            carousel.start()
+
+            vi.advanceTimersByTime(timer * 1);
+            expect(value).toBe(value % index);
+
+            vi.advanceTimersByTime(timer * 2);
+            expect(value).toBe(value % index);
+        });
+        
+        it('Index does not go out of bound', () => {
+            const [carousel] = dummyApp(() => useRotateCollections(index, timer * 6))
+            const value = carousel.index.value;
+            carousel.start()
+
+            vi.advanceTimersByTime(timer * 1);
+            expect(value).greaterThan(-1);
+            expect(value).lessThan(index + 1);
+
+            vi.advanceTimersByTime(timer * 6)
+            expect(value).greaterThanOrEqual(0);
+            expect(value).lessThanOrEqual(index);
         });
     });
     describe('useNavigation()', () => {
