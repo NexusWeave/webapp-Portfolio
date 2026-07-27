418e6ae884864ed8fc7b9689d96103ae855efd6c
krigjo25 <krigjo25@gmail.com>
2026-07-27 23:02:10 +0200

    feat(seo): add Schema.org JSON-LD structured data for Sitelinks

--- frontend/app.vue
+++ frontend/app.vue
@@ -14,6 +14,81 @@ useSeoMeta({
   twitterDescription: 'Portefølje side for Kristoffer Gjøsund',
   twitterImage: 'https://krigjo25.no/og-image.png'
 })
+
+useHead({
+  script: [
+    {
+      type: 'application/ld+json',
+      children: JSON.stringify({
+        '@context': 'https://schema.org',
+        '@graph': [
+          {
+            '@type': 'WebSite',
+            '@id': 'https://krigjo25.no/#website',
+            'url': 'https://krigjo25.no/',
+            'name': 'Kristoffer Gjøsund - Portefølje',
+            'description': 'Portefølje side for Kristoffer Gjøsund (krigjo25)',
+            'inLanguage': 'nb-NO',
+            'publisher': {
+              '@type': 'Person',
+              '@id': 'https://krigjo25.no/#person',
+              'name': 'Kristoffer Gjøsund',
+              'url': 'https://krigjo25.no',
+              'sameAs': [
+                'https://github.com/krigjo25'
+              ]
+            },
+            'hasPart': [
+              {
+                '@type': 'WebPage',
+                '@id': 'https://krigjo25.no/#developer',
+                'url': 'https://krigjo25.no/dev',
+                'name': 'Utvikler & IT',
+                'description': 'Utviklingsprosjekter, erfaring og teknologisk kompetanse'
+              },
+              {
+                '@type': 'WebPage',
+                '@id': 'https://krigjo25.no/#personal',
+                'url': 'https://krigjo25.no/personal',
+                'name': 'Personlig & Om meg',
+                'description': 'Informasjon om Kristoffer Gjøsund sin bakgrunn'
+              },
+              {
+                '@type': 'WebPage',
+                '@id': 'https://krigjo25.no/#logs',
+                'url': 'https://krigjo25.no/logs',
+                'name': 'Tekniske logger & Artikler',
+                'description': 'Artikler, logger og dokumentasjon om IT, KI og utvikling'
+              }
+            ]
+          },
+          {
+            '@type': 'SiteNavigationElement',
+            '@id': 'https://krigjo25.no/#navigation',
+            'name': 'Hovedmeny',
+            'hasPart': [
+              {
+                '@type': 'WebPage',
+                'name': 'Utvikler',
+                'url': 'https://krigjo25.no/dev'
+              },
+              {
+                '@type': 'WebPage',
+                'name': 'Personlig',
+                'url': 'https://krigjo25.no/personal'
+              },
+              {
+                '@type': 'WebPage',
+                'name': 'Logger',
+                'url': 'https://krigjo25.no/logs'
+              }
+            ]
+          }
+        ]
+      })
+    }
+  ]
+})
 </script>
 
 <template>
