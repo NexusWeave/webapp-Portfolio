2c3056eb744efb339d32d70b66ce3aab02c4eae5
krigjo25 <krigjo25@gmail.com>
2026-07-28 22:03:02 +0200

    refactor(typedefinition): Add Datetime typedefinition.

--- frontend/types/documents.d.ts
+++ frontend/types/documents.d.ts
@@ -1,10 +1,11 @@
 //  --- Import types logic
 
 import type { Anchor } from '~/types/anchor';
+import type { DateItem } from '~/types/date';
 
 interface DocumentItem { id: number; title: string; body: Record<string, any>; anchor: Anchor; }
 
 export interface ReferenceItem extends DocumentItem {}
 export interface PostTag { name: string; href: string; label: string; cls: string[];  type: string[]; labels?:string[] };
-export interface PostItem extends DocumentItem {  tags: PostTag[]; path: string;  anchor: Anchor; date: DateItem;  status: string; ingress: string; sources?: string;  isArchived: boolean; isPublished: boolean; }
+export interface PostItem extends DocumentItem {  tags: PostTag[]; path: string;  anchor: Anchor; date: DateItem;  status: string; ingress: string; info?: string; sources?: string;  isArchived: boolean; isPublished: boolean; }
 export interface PostProps { data: PostItem ; cls?: string[]; }
