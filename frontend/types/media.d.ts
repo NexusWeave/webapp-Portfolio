f5f412506cd893b53795a0474b6f153c81cf5e5f
krigjo25 <krigjo25@gmail.com>
2026-08-07 15:18:21 +0200

    refactor(media): Updating typedefinitions for FigureItem.

--- frontend/types/media.d.ts
+++ frontend/types/media.d.ts
@@ -3,9 +3,11 @@ export interface FigureItem
     src : string;
     alt : string;
     type : string;
+    label? : string;
     srcset? : string;
     caption? : string;
-    label? : string;
+    width? : number | string;
+    height? : number | string;
 }
 export interface iconProps { cls?: string[]; }
 export interface FigureProps { cls?   : string[]; data    : FigureItem; }
